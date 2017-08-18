4a84e334d8b68d8cf86fc8b371b67be83f2debf4
shobhit bhardwaj <shobhit.bhardwaj@gaussnetworks.com>
2017-08-18 15:48:47 +0530

    Java Thread

---
+++ src/com/concurrent/executorFramework/CountDownLatchTest.java
@@ -0,0 +1,64 @@
+package com.concurrent.executorFramework;
+
+import java.util.concurrent.CountDownLatch;
+
+
+class Waiter extends Thread {
+	private CountDownLatch latch;
+
+	public Waiter(CountDownLatch latch) {
+		this.latch = latch;
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Waiter await() Start");
+		try {
+			latch.await();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		System.out.println("Waiter await() End");
+	}
+}
+
+class Decrementer extends Thread {
+	private CountDownLatch latch;
+
+	public Decrementer(CountDownLatch latch) {
+		this.latch = latch;
+	}
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(1000);
+			latch.countDown();
+			System.out.println("Count Down Sleep 1");
+
+			Thread.sleep(1000);
+			latch.countDown();
+			System.out.println("Count Down Sleep 2");
+
+			Thread.sleep(1000);
+			latch.countDown();
+			System.out.println("Count Down Sleep 3");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
+
+public class CountDownLatchTest {
+	public static void main(String[] args) throws Exception {
+		CountDownLatch latch = new CountDownLatch(3);
+
+		Waiter waiter = new Waiter(latch);
+		Decrementer decrementer = new Decrementer(latch);
+		waiter.start();
+		decrementer.start();
+		System.out.println("Maid Thread Sleep Start");
+		Thread.sleep(5000);
+		System.out.println("Maid Thread Sleep End");
+	}
+}
