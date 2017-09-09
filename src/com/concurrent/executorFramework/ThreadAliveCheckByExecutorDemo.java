3135ddc608e0fb51f79bfd864b5de858e1cb8f2b
Shobhit Bhardwaj <successshobhit@gmail.com>
2017-09-09 18:19:08 +0530

    Thread Alive Check By Executor

--- src/com/concurrent/executorFramework/ThreadAliveCheckByExecutorDemo.java
+++ src/com/concurrent/executorFramework/ThreadAliveCheckByExecutorDemo.java
@@ -1,6 +1,5 @@
 package com.concurrent.executorFramework;
 
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
