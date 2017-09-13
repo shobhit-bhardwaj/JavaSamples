9d799173ff55fec7f2aff1af3823b34d14d60773
shobhit bhardwaj <shobhit.bhardwaj@gaussnetworks.com>
2017-09-13 16:06:24 +0530

    Atomic Data Types

--- src/com/concurrent/AtomicDataTypes.java
+++ src/com/concurrent/AtomicDataTypes.java
@@ -1,9 +1,11 @@
 package com.concurrent;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class AtomicDataTypes {
-	private AtomicBoolean atomicBoolean = new AtomicBoolean(true);	//	By Default false
+	private AtomicBoolean atomicBoolean = new AtomicBoolean(true);		//	By Default false
+	private AtomicInteger atomicInteger = new AtomicInteger(15);		//	By Default 0
 
 	public void atomicBooleanOperations() {
 		System.out.println("Initial Value - "+atomicBoolean.get());
@@ -12,12 +14,45 @@ public class AtomicDataTypes {
 		System.out.println("After Setting false - "+atomicBoolean.get());
 
 		boolean previousValue = atomicBoolean.getAndSet(true);
-		System.out.println("Previous Value - "+previousValue+" | Current Value - "+atomicBoolean.get());
+		System.out.println("getAndSet() Previous Value - "+previousValue+" | Current Value - "+atomicBoolean.get());
 
 		boolean expectedValue = true;
 		boolean updateValue = false;
 		boolean success = atomicBoolean.compareAndSet(expectedValue, updateValue);
 		System.out.println("Compare and Set Status - "+success+" | Updated Value - "+atomicBoolean.get());
+	}
+
+	public void atomicIntegerOperations() {
+		System.out.println("Initial Value - "+atomicInteger.get());
+
+		atomicInteger.set(25);
+		System.out.println("After Setting 25 - "+atomicInteger.get());
+
+		int previousValue = atomicInteger.getAndSet(35);
+		System.out.println("getAndSet() Previous Value - "+previousValue+" | Current Value - "+atomicInteger.get());
+
+		int expectedValue = 35;
+		int updateValue = 45;
+		boolean success = atomicInteger.compareAndSet(expectedValue, updateValue);
+		System.out.println("Compare and Set Status - "+success+" | Updated Value - "+atomicInteger.get());
+
+		previousValue = atomicInteger.getAndIncrement();
+		System.out.println("getAndIncrement() Previous Value - "+previousValue+" | Current Value - "+atomicInteger.get());
+
+		previousValue = atomicInteger.getAndDecrement();
+		System.out.println("getAndDecrement() Previous Value - "+previousValue+" | Current Value - "+atomicInteger.get());
+
+		previousValue = atomicInteger.incrementAndGet();
+		System.out.println("incrementAndGet() Previous Value - "+previousValue+" | Current Value - "+atomicInteger.get());
+
+		previousValue = atomicInteger.decrementAndGet();
+		System.out.println("decrementAndGet() Previous Value - "+previousValue+" | Current Value - "+atomicInteger.get());
+
+		previousValue = atomicInteger.getAndAdd(10);
+		System.out.println("getAndAdd() Previous Value - "+previousValue+" | Current Value - "+atomicInteger.get());
+
+		previousValue = atomicInteger.addAndGet(10);
+		System.out.println("addAndGet() Previous Value - "+previousValue+" | Current Value - "+atomicInteger.get());
 	}
 
 	public static void main(String[] args) {
@@ -26,5 +61,8 @@ public class AtomicDataTypes {
 		System.out.println("--- AtomicBoolean Operations ---");
 		object.atomicBooleanOperations();
 		System.out.println();
+
+		System.out.println("--- AtomicInteger Operations ---");
+		object.atomicIntegerOperations();
 	}
 }
