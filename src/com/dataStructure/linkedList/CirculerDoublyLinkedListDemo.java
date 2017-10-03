c11e53c28686a723ecdee61a13ed09db20cdf69f
shobhit bhardwaj <shobhit.bhardwaj@gaussnetworks.com>
2017-10-03 18:37:42 +0530

    Circuler Doubly Linked List

---
+++ src/com/dataStructure/linkedList/CirculerDoublyLinkedListDemo.java
@@ -0,0 +1,151 @@
+package com.dataStructure.linkedList;
+
+public class CirculerDoublyLinkedListDemo {
+	private class Node {
+		private int data;
+		private Node next;
+		private Node prev;
+
+		public Node(int data) {
+			this.data = data;
+		}
+	}
+
+	private Node head;
+	private Node tail;
+
+	public void insertData(int data) {
+		Node node = new Node(data);
+
+		if(head == null) {
+			head = node;
+			tail = node;
+		} else {
+			node.next = head;
+			node.prev = tail;
+			head.prev = node;
+			tail.next = node;
+			tail = node;
+		}
+	}
+
+	public void insertData(int data, int position) {
+		if(position < 0)
+			position = 0;
+
+		int size = size();
+		if(position > size)
+			position = size;
+
+		Node node = new Node(data);
+
+		if(head == null) {
+			head = node;
+			tail = node;
+		} else {
+			if(position == 0) {
+				node.next = head;
+				node.prev = tail;
+				head.prev = node;
+				tail.next = node;
+				head = node;
+			} else if(position == size) {
+				tail.next = node;
+				node.prev = tail;
+				node.next = head;
+				head.prev = node;
+				tail = node;
+			} else {
+				Node temp = head;
+				for(int i=0; i<position-1; i++) {
+					temp = temp.next;
+				}
+				node.prev = temp;
+				node.next = temp.next;
+				temp.next.prev = node;
+				temp.next = node;
+			}
+		}
+	}
+
+	public void deleteData(int position) {
+		if(position < 0)
+			position = 0;
+
+		int size = size();
+		if(position > size)
+			position = size;
+
+		if(position == 0) {
+			tail.next = tail.next.next;
+			tail.next.prev = tail;
+			head = tail.next;
+		} else if(position == size) {
+			head.prev = head.prev.prev;
+			head.prev.next = head;
+			tail = head.prev;
+		} else {
+			Node temp = head;
+			for(int i=0; i<position-1; i++) {
+				temp = temp.next;
+			}
+			temp.next = temp.next.next;
+			temp.next.prev = temp;
+		}
+	}
+
+	public void traverse(boolean forward) {
+		if(forward) {
+			Node temp = head;
+			do {
+				System.out.print(temp.data+"\t");
+				temp = temp.next;
+			} while (temp != head);
+		} else {
+			Node temp = tail;
+			do {
+				System.out.print(temp.data+"\t");
+				temp = temp.prev;
+			} while (temp != tail);
+		}
+
+		System.out.println();
+	}
+
+	public int size() {
+		int count = 0;
+
+		Node temp = head;
+		do {
+			count++;
+			temp = temp.next;
+		} while(temp != head);
+
+		return count;
+	}
+
+	public void test() {
+		insertData(10);
+		insertData(20);
+		insertData(30);
+		insertData(40);
+		insertData(50);
+		insertData(60, 0);
+		insertData(70, 4);
+		insertData(80, 9);
+		traverse(true);
+		traverse(false);
+		System.out.println("Size - "+size());
+
+		deleteData(4);
+		deleteData(0);
+		deleteData(9);
+		traverse(true);
+		traverse(false);
+		System.out.println("Size - "+size());
+	}
+
+	public static void main(String[] args) {
+		new CirculerDoublyLinkedListDemo().test();
+	}
+}
