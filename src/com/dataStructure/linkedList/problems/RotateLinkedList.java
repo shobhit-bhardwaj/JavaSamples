3e0c937a02391c2b7d43d9a4fd7471fdfcc82595
shobhit bhardwaj <shobhit.bhardwaj@gaussnetworks.com>
2017-10-17 15:49:05 +0530

    Rotate Linked List

---
+++ src/com/dataStructure/linkedList/problems/RotateLinkedList.java
@@ -0,0 +1,41 @@
+package com.dataStructure.linkedList.problems;
+
+import com.dataStructure.linkedList.problems.LinkedListDemo.Node;
+
+public class RotateLinkedList {
+	public Node rotate(Node head, int k) {
+		Node newHead = null;
+
+		int counter = 1;
+		Node current = head;
+		Node prev = null;
+		while(current.next != null) {
+			if(counter == k) {
+				newHead = current;
+				prev.next = null;
+			}
+			counter++;
+			prev = current;
+			current = current.next;
+		}
+		current.next = head;
+		head = newHead;
+
+		return head;
+	}
+
+	public static void main(String[] args) {
+		LinkedListDemo linkedList = new LinkedListDemo();
+		linkedList.insertData(10);
+		linkedList.insertData(20);
+		linkedList.insertData(30);
+		linkedList.insertData(40);
+		linkedList.insertData(50);
+		Node head = linkedList.getHead();
+		linkedList.traverse(head);
+
+		RotateLinkedList rotateLinkedList = new RotateLinkedList();
+		head = rotateLinkedList.rotate(head, 3);
+		linkedList.traverse(head);
+	}
+}
