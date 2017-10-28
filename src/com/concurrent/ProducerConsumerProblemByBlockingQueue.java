18c670b5b514a328a6e4cab3e85d1a3dbb8dad3e
Shobhit Bhardwaj <successshobhit@gmail.com>
2017-10-28 17:01:47 +0530

    Producer Consumer Problem by Blocking Queue

---
+++ src/com/concurrent/ProducerConsumerProblemByBlockingQueue.java
@@ -0,0 +1,52 @@
+package com.concurrent;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+class ProducerThread extends Thread {
+	private BlockingQueue<Integer> queue;
+
+	public ProducerThread(BlockingQueue<Integer> queue) {
+		this.queue = queue;
+	}
+
+	@Override
+	public void run() {
+		for(int i=1; i<=10; i++) {
+			try {
+				queue.put(i);
+				System.out.println("Produced - "+i);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
+
+class ConsumerThread extends Thread {
+	private BlockingQueue<Integer> queue;
+
+	public ConsumerThread(BlockingQueue<Integer> queue) {
+		this.queue = queue;
+	}
+
+	@Override
+	public void run() {
+		for(int i=1; i<=10; i++) {
+			try {
+				int data = queue.take();
+				System.out.println("Consumed - "+data);
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
+
+public class ProducerConsumerProblemByBlockingQueue {
+	public static void main(String[] args) {
+		BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(1);
+		new ProducerThread(queue).start();
+		new ConsumerThread(queue).start();
+	}
+}
