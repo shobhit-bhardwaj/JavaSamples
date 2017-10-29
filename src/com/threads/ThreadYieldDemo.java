b2e29c8324e4165a4baf6fa169a222a944e3fec9
Shobhit Bhardwaj <successshobhit@gmail.com>
2017-10-29 14:54:41 +0530

    Thread Yield Demo

---
+++ src/com/threads/ThreadYieldDemo.java
@@ -0,0 +1,34 @@
+package com.threads;
+
+class FirstTask extends Thread {
+	@Override
+	public void run() {
+		for(int i=1; i<=5; i++) {
+			Thread.yield();
+			System.out.println("First Task - "+i);
+		}
+	}
+}
+
+class SecondTask extends Thread {
+	@Override
+	public void run() {
+		for(int i=1; i<=5; i++) {
+			Thread.yield();
+			System.out.println("Second Task - "+i);
+		}
+	}
+}
+
+public class ThreadYieldDemo {
+	public static void main(String[] args) {
+		Thread firstTask = new FirstTask();
+		firstTask.setPriority(Thread.MIN_PRIORITY);
+
+		Thread secondTask = new SecondTask();
+		secondTask.setPriority(Thread.MAX_PRIORITY);
+
+		firstTask.start();
+		secondTask.start();
+	}
+}
