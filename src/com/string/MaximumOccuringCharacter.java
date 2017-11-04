6883d55067f00e84d40cf244ecf60e905eb71245
Shobhit Bhardwaj <successshobhit@gmail.com>
2017-11-04 21:25:56 +0530

    Maximum Occuring Character

---
+++ src/com/string/MaximumOccuringCharacter.java
@@ -0,0 +1,25 @@
+package com.string;
+
+public class MaximumOccuringCharacter {
+	public static void main(String[] args) {
+		String input = "hellointheworldofjava";
+
+		int[] array = new int[256];
+		char[] charArray = input.toCharArray();
+		for(char ch : charArray) {
+			int value = array[ch];
+			value++;
+			array[ch] = value;
+		}
+
+		int max = -1;
+		char result = ' ';
+		for(int i=0; i<array.length; i++) {
+			if(array[i] > max) {
+				max = array[i];
+				result = (char) i;
+			}
+		}
+		System.out.println(result);
+	}
+}
