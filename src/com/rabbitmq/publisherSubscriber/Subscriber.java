c83f0a5d74582050ba8a17ef758d53480eb865a8
shobhit bhardwaj <shobhit.bhardwaj@gaussnetworks.com>
2017-12-06 16:31:43 +0530

    Rabbit MQ - Publisher Subscriber

---
+++ src/com/rabbitmq/publisherSubscriber/Subscriber.java
@@ -0,0 +1,36 @@
+package com.rabbitmq.publisherSubscriber;
+
+import java.io.IOException;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+public class Subscriber {
+	private static final String EXCHANGE_NAME = "testExchange";
+
+	public static void main(String[] args) throws Exception {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost("localhost");
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+		String queueName = channel.queueDeclare().getQueue();
+		channel.queueBind(queueName, EXCHANGE_NAME, "");
+
+		System.out.println("Waiting for Messages. To Exit, Press CTRL+C");
+
+		Consumer consumer = new DefaultConsumer(channel) {
+			@Override
+			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+				String message = new String(body, "UTF-8");
+				System.out.println("Message Receive - "+message);
+			}
+		};
+		channel.basicConsume(queueName, true, consumer);
+	}
+}
