61a6f16cddcc188be3dc951d162a7e4e12d47561
shobhit bhardwaj <shobhit.bhardwaj@gaussnetworks.com>
2017-12-15 16:12:30 +0530

    Java Socket - Simple Client Server

---
+++ src/com/coreJava/socket/simple/SimpleServer.java
@@ -0,0 +1,33 @@
+package com.coreJava.socket.simple;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SimpleServer {
+	public static void main(String[] args) {
+		try {
+			ServerSocket serverSocket = new ServerSocket(6666);
+			Socket socket = serverSocket.accept();
+
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			String message;
+			while((message = in.readLine()) != null) {
+				System.out.println("Message from Client - "+message);
+				String response = "Hello Client";
+				out.println(response);
+			}
+
+			socket.close();
+			serverSocket.close();
+			in.close();
+			out.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
