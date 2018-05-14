7cefbd0bcf505f0f59bae57c120b02821108b27d
a1000891 <Ru$43876>
2018-05-14 13:23:07 +0530

    Join Multiple Arrays

---
+++ src/com/java8/examples/JoinMultipleArrays.java
@@ -0,0 +1,15 @@
+package com.java8.examples;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class JoinMultipleArrays {
+	public static void main(String[] args) {
+		int[] array1 = new int[] {1, 2, 3, 4, 5};
+		int[] array2 = new int[] {6, 7};
+		int[] array3 = new int[] {8, 9, 10};
+
+		int[] result = IntStream.concat(Arrays.stream(array1), IntStream.concat(Arrays.stream(array2), Arrays.stream(array3))).toArray();
+		System.out.println("Result Array - "+Arrays.toString(result));
+	}
+}
