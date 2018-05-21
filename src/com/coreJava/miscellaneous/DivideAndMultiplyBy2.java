fda4a1b05f5b86dc5005572dce99c9a5e2f04f34
a1000891 <Ru$43876>
2018-05-21 11:28:54 +0530

    Multiply And Divide By 2 Using Shift Operator

---
+++ src/com/coreJava/miscellaneous/DivideAndMultiplyBy2.java
@@ -0,0 +1,11 @@
+package com.coreJava.miscellaneous;
+
+public class DivideAndMultiplyBy2 {
+	public static void main(String[] args) {
+		int number = 10;
+
+		System.out.println("Number is - " + number);
+		System.out.println("Multiply By 2 - " + (number << 1));
+		System.out.println("Divide By 2 - " + (number >> 1));
+	}
+}
