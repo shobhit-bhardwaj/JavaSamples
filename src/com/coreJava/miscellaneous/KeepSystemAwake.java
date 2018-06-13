99b0c40fc5214bba2df8a4c2ae8218d2a929269a
a1000891 <Ru$43876>
2018-06-13 10:33:47 +0530

    Keep System Awake

---
+++ src/com/coreJava/miscellaneous/KeepSystemAwake.java
@@ -0,0 +1,17 @@
+package com.coreJava.miscellaneous;
+
+import java.awt.Robot;
+import java.util.Random;
+
+public class KeepSystemAwake {
+	public static void main(String[] args) throws Exception {
+		Robot hal = new Robot();
+        Random random = new Random();
+        while(true){
+            hal.delay(1000 * 10);
+            int x = random.nextInt() % 640;
+            int y = random.nextInt() % 480;
+            hal.mouseMove(x,y);
+        }
+	}
+}
