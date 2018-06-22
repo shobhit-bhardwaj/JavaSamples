76e10c6f9fa4c9c67730cc0883457bec647f6232
A1000891 <A1000891@AINLH5CG64933BZ.hewitt.com>
2018-06-22 14:57:54 +0530

    Optional Example, Use of isPresent orElse

---
+++ src/com/java8/examples/OptionalFindBook.java
@@ -0,0 +1,21 @@
+package com.java8.examples;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalFindBook {
+	private static final List<String> books = Arrays.asList("Effective Java", "Java Made Easy", "Core Java Basics");
+
+	private static String getBook(String letter) {
+		Optional<String> bookFound = books.stream().filter(book -> book.startsWith(letter)).findFirst();
+
+		//return bookFound.isPresent() ? bookFound.get() : "Book Not Found";
+		return bookFound.orElse("Book Not Found");
+	}
+
+	public static void main(String[] args) {
+		String book = getBook("J");
+		System.out.println(book);
+	}
+}
