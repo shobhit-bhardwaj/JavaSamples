f27d8da53b2b05a9cab0752d5dff3f507d46e2cc
A1000891 <A1000891@AINLH5CG64933BZ.hewitt.com>
2018-06-22 14:58:40 +0530

    Keep System Awake

--- src/com/coreJava/miscellaneous/KeepSystemAwake.java
+++ src/com/coreJava/miscellaneous/KeepSystemAwake.java
@@ -8,7 +8,7 @@ public class KeepSystemAwake {
 		Robot hal = new Robot();
         Random random = new Random();
         while(true){
-            hal.delay(1000 * 10);
+            hal.delay(1000 * 60);
             int x = random.nextInt() % 640;
             int y = random.nextInt() % 480;
             hal.mouseMove(x,y);
