e2795e637e5bac09eae05430c356df4c83c95ddf
a1000891 <Ru$43876>
2018-07-09 16:23:46 +0530

    Count Occurrence of Character in String

---
+++ src/com/dataStructure/string/CountOccurrenceOfCharacter.java
@@ -0,0 +1,12 @@
+package com.dataStructure.string;
+
+public class CountOccurrenceOfCharacter {
+	public static void main(String[] args) {
+		String str = "hello world";
+		char letter = 'l';
+
+		long count = str.chars().filter(ch -> ch == letter).count();
+
+		System.out.println("Occurrence of Character - "+count);
+	}
+}
