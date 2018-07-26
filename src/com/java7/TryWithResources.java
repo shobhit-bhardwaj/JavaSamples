ce3cad2a88c840c44b9c9bb3fe1eff5ccb6501b9
a1000891 <Ru$43876>
2018-07-26 12:29:00 +0530

    Try With Resources

---
+++ src/com/java7/TryWithResources.java
@@ -0,0 +1,19 @@
+package com.java7;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class TryWithResources {
+	public static void main(String[] args) {
+		try (
+			FileInputStream inputStream = new FileInputStream("tmp.txt");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))
+		) {
+			inputStream.close();
+			reader.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
