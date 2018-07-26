c58482acd3d45935a44f1b8b5254568b51e9faa4
a1000891 <Ru$43876>
2018-07-26 13:12:20 +0530

    File Change Notifications

---
+++ src/com/java7/FileChangeNotifications.java
@@ -0,0 +1,42 @@
+package com.java7;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchEvent.Kind;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+
+public class FileChangeNotifications {
+	public static void main(String[] args) {
+		Path path = Paths.get("C:\\Temps\\");
+		WatchService watchService = null;
+		try {
+			watchService = FileSystems.getDefault().newWatchService();
+			path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE,
+					StandardWatchEventKinds.ENTRY_MODIFY);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		WatchKey key = null;
+		while (true) {
+			try {
+				key = watchService.take();
+				for (WatchEvent<?> event : key.pollEvents()) {
+					Kind<?> kind = event.kind();
+					System.out.println("Event on - " + event.context().toString() + " | is - " + kind);
+				}
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+
+			boolean reset = key.reset();
+			if (!reset)
+				break;
+		}
+	}
+}
