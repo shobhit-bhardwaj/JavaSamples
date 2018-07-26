5f9d851a91f2cc0e2f0d1506419c46e4861b368d
a1000891 <Ru$43876>
2018-07-26 10:17:44 +0530

    Binary Literals

---
+++ src/com/java7/BinaryLiterals.java
@@ -0,0 +1,11 @@
+package com.java7;
+
+public class BinaryLiterals {
+	public static void main(String[] args) {
+		int binary = 0b10010;
+		System.out.println(binary);
+
+		binary = 10_01_70_365;
+		System.out.println(binary);
+	}
+}
