93b0452f27fbe07bb4716e3197580f46ccf3f007
a1000891 <Ru$43876>
2018-07-26 10:20:56 +0530

    Diamond Syntax

---
+++ src/com/java7/DiamondSyntax.java
@@ -0,0 +1,16 @@
+package com.java7;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DiamondSyntax {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+		System.out.println(list);
+
+		Map<Integer, String> map = new HashMap<>();
+		System.out.println(map);
+	}
+}
