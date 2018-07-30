8244ab9abaaf81d8f834919036e29a9385193f92
a1000891 <Ru$43876>
2018-07-30 12:11:52 +0530

    Version String Scheme

---
+++ src/com/java9/VersionStringScheme.java
@@ -0,0 +1,14 @@
+package com.java9;
+
+public class VersionStringScheme {
+	public static void main(String[] args) {
+		Runtime.Version version = Runtime.version();
+
+		System.out.println("Current Version - " + version);
+		System.out.println("Major Version - " + version.major());
+		System.out.println("Minor Version - " + version.minor());
+		System.out.println("Security Version - " + version.security());
+		System.out.println("Pre-Released - " + version.pre().orElse("None"));
+		System.out.println("Build Number - " + version.build().get());
+	}
+}
