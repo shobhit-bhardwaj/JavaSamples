db4a3569c5542120a33911461a3a6cf02a7ea480
a1000891 <Ru$43876>
2018-07-30 12:02:06 +0530

    SafeVarargs Annonation

---
+++ src/com/java9/SafeVarargsAnnonation.java
@@ -0,0 +1,20 @@
+package com.java9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SafeVarargsAnnonation {
+	@SafeVarargs
+	public static void display(List<String>... lists) {
+		System.out.println(lists);
+	}
+
+	public static void main(String[] args) {
+		List<String> strings = new ArrayList<>();
+		strings.add("AAA");
+		strings.add("BBB");
+		strings.add("CCC");
+
+		display(strings);
+	}
+}
