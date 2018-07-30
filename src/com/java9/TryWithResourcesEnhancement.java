93e14a127ccb0f9bd7968b9aade5b7ed38afeaa2
a1000891 <Ru$43876>
2018-07-30 11:44:53 +0530

    Try With Resources Enhancement

---
+++ src/com/java9/TryWithResourcesEnhancement.java
@@ -0,0 +1,15 @@
+package com.java9;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class TryWithResourcesEnhancement {
+	public static void main(String[] args) throws IOException {
+		FileInputStream inputStream = new FileInputStream("tmp.txt");
+		try (inputStream) {
+			inputStream.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
