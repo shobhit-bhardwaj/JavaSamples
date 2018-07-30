595f67dd212847e4e4a8f97fca1f5e3a0af784b8
a1000891 <Ru$43876>
2018-07-30 12:34:59 +0530

    Remove Underscore as a Variable

---
+++ src/com/java9/RemoveUnderscoreAsVariable.java
@@ -0,0 +1,17 @@
+package com.java9;
+
+public class RemoveUnderscoreAsVariable {
+	public static void main(String[] args) {
+		/*
+		 * This code can be Compile in Java7 Without Error or Warning,
+		 * In Java8, It Gives Only Warning,
+		 * In Java9, This Code Wont Compile.
+		 * 
+		int _ = 10;
+		System.out.println(_);
+		*/
+
+		int _a = 10;
+		System.out.println(_a);
+	}
+}
