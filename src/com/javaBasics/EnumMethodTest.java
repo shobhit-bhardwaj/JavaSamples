b7926cbb5267cd283b921042cc594432069bc024
a1000891 <Ru$43876>
2018-08-20 12:50:43 +0530

    Enum Method Test

---
+++ src/com/javaBasics/EnumMethodTest.java
@@ -0,0 +1,36 @@
+package com.javaBasics;
+
+enum Direction {
+	EAST {
+		@Override
+		public void display() {
+			System.out.println("East");
+		}
+	},
+	WEST {
+		@Override
+		public void display() {
+			System.out.println("West");
+		}
+	},
+	NORTH {
+		@Override
+		public void display() {
+			System.out.println("North");
+		}
+	},
+	SOUTH {
+		@Override
+		public void display() {
+			System.out.println("South");
+		}
+	};
+
+	public abstract void display();
+}
+
+public class EnumMethodTest {
+	public static void main(String[] args) {
+		Direction.EAST.display();
+	}
+}
