d60d43321d3c3020e15a65f1197843fc0db0a471
A1000891 <A1000891@AINLH5CG64933BZ.hewitt.com>
2018-11-23 15:17:02 +0530

    Even Odd Generator

--- src/com/threads/EvenOddGenerator.java
+++ src/com/threads/EvenOddGenerator.java
@@ -1,67 +1,77 @@
 package com.threads;
 
+import java.util.concurrent.TimeUnit;
+
 class BufferEvenOdd {
-	private boolean even = false;
+	private volatile boolean even = false;
 
 	public synchronized void printEven(int number) {
-		if(even) {
+		while (even) {
 			try {
 				wait();
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
 		}
-		System.out.println("Even - "+number);
+		System.out.println("Even - " + number);
 		even = true;
-		notify();
+		notifyAll();
 	}
 
 	public synchronized void printOdd(int number) {
-		if(!even) {
+		while (!even) {
 			try {
 				wait();
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
 		}
-		System.out.println("Odd - "+number);
+		System.out.println("Odd - " + number);
 		even = false;
-		notify();
+		notifyAll();
 	}
 }
 
 class EvenGenerator extends Thread {
 	private BufferEvenOdd buffer;
+	private int number;
 
-	public EvenGenerator(BufferEvenOdd buffer) {
+	public EvenGenerator(BufferEvenOdd buffer, int number) {
 		this.buffer = buffer;
+		this.number = number;
 	}
 
 	@Override
 	public void run() {
-		for(int i=0; i<=10; i+=2)
-			buffer.printEven(i);
+		buffer.printEven(number);
 	}
 }
 
 class OddGenerator extends Thread {
 	private BufferEvenOdd buffer;
+	private int number;
 
-	public OddGenerator(BufferEvenOdd buffer) {
+	public OddGenerator(BufferEvenOdd buffer, int number) {
 		this.buffer = buffer;
+		this.number = number;
 	}
 
 	@Override
 	public void run() {
-		for(int i=1; i<=9; i+=2)
-			buffer.printOdd(i);
+		buffer.printOdd(number);
 	}
 }
 
 public class EvenOddGenerator {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		BufferEvenOdd buffer = new BufferEvenOdd();
-		new EvenGenerator(buffer).start();
-		new OddGenerator(buffer).start();
+
+		for(int i=0; i<=10; i+=2)
+			new EvenGenerator(buffer, i).start();
+
+		TimeUnit.SECONDS.sleep(1);
+
+		for(int i=1; i<=9; i+=2)
+			new OddGenerator(buffer, i).start();
 	}
 }
