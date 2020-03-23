7cf771e90d0cf15a6499d51e24f33493454bcd7c
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-03-23 10:09:00 +0530

    FixedThreadPool Demo

--- src/com/multithreading/concurrent/executorFramework/FixedSizeThreadPoolDemo.java
+++ src/com/multithreading/concurrent/executorFramework/FixedSizeThreadPoolDemo.java
@@ -2,20 +2,29 @@ package com.multithreading.concurrent.executorFramework;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class FixedSizeThreadPoolDemo {
-	public static void main(String[] args) {
-		System.out.println("Main Thread Starts.");
+	private static class SimpleThread implements Runnable {
+		@Override
+		public void run() {
+			try {
+				for(int i=0; i<5; i++) {
+					TimeUnit.SECONDS.sleep(1);
+					System.out.println("Counter - " + (i + 1) + " - By Thread - " + Thread.currentThread().getName());
+				}
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
 
+	public static void main(String[] args) {
 		ExecutorService executor = Executors.newFixedThreadPool(3);
-		executor.submit(new SimpleThreadTask());
-		executor.submit(new SimpleThreadTask());
-		executor.submit(new SimpleThreadTask());
-		//executor.submit(new SimpleThreadTask());
-		//executor.submit(new SimpleThreadTask());
+
+		for(int i=0; i<5; i++)
+			executor.submit(new SimpleThread());
 
 		executor.shutdown();
-
-		System.out.println("Main Thread Done.");
 	}
 }
