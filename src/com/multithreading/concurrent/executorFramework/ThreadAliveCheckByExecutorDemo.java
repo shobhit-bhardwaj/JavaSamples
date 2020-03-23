3e4c8493eb243a0abb4c88a54c21e4c1b66f1a4e
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-03-23 13:19:14 +0530

    Thread Alive Check by Executors

--- src/com/multithreading/concurrent/executorFramework/ThreadAliveCheckByExecutorDemo.java
+++ src/com/multithreading/concurrent/executorFramework/ThreadAliveCheckByExecutorDemo.java
@@ -5,53 +5,50 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
-class ThreadAliveTask implements Runnable {
-	private int delay;
-
-	public ThreadAliveTask(int delay) {
-		this.delay = delay;
-	}
-
-	@Override
-	public void run() {
-		for (int i = 1; i <= 10; i++) {
-			System.out.println(Thread.currentThread().getName() + " - counter - " + i);
-			try {
-				TimeUnit.MILLISECONDS.sleep(delay);
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-		}
-	}
-}
-
 public class ThreadAliveCheckByExecutorDemo {
-	public static void main(String[] args) {
-		ExecutorService executorService = Executors.newCachedThreadPool();
-		Future<?> future1 = executorService.submit(new ThreadAliveTask(600));
-		Future<Integer> future2 = executorService.submit(new ThreadAliveTask(800), 300);
+	private static class SimpleRunnable implements Runnable {
+		private int delay;
 
-		while (true) {
-			System.out.println("First Task Status - " + future1.isDone());
-			System.out.println("Second Task Status - " + future2.isDone());
+		public SimpleRunnable(int delay) {
+			this.delay = delay;
+		}
 
+		@Override
+		public void run() {
 			try {
-				TimeUnit.MILLISECONDS.sleep(500);
+				System.out.println("SimpleCallable Thread running by Thread - " + Thread.currentThread().getName());
+				TimeUnit.SECONDS.sleep(delay);
 			} catch (InterruptedException ex) {
 				ex.printStackTrace();
 			}
+		}
+	}
 
-			if(future1.isDone() && future2.isDone())
+	public static void main(String[] args) throws Exception {
+		SimpleRunnable runnable1 = new SimpleRunnable(4);
+		SimpleRunnable runnable2 = new SimpleRunnable(7);
+
+		ExecutorService service = Executors.newCachedThreadPool();
+		Future<?> future1 = service.submit(runnable1);
+		Future<Integer> future2 = service.submit(runnable2, 100);
+
+		while(true) {
+			TimeUnit.MILLISECONDS.sleep(500);
+
+			boolean done1 = future1.isDone();
+			boolean done2 = future2.isDone();
+
+			System.out.println("First Task Done - " + done1);
+			System.out.println("Second Task Done - " + done2);
+
+			if(done1 && done2)
 				break;
 		}
 
-		try {
-			System.out.println("Result 1 - "+future1.get());
-			System.out.println("Result 2 - "+future2.get());
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
+		System.out.println("Both Task Completed.");
+		System.out.println("First Task Returns - " + future1.get());
+		System.out.println("Second Task Returns - " + future2.get());
 
-		executorService.shutdown();
+		service.shutdown();
 	}
 }
