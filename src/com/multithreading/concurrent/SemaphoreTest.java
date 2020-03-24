5ceafed6d618859cec0f3e8da9c1270a66285ee5
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-03-24 09:04:06 +0530

    Semaphore acquire and release

--- src/com/multithreading/concurrent/SemaphoreTest.java
+++ src/com/multithreading/concurrent/SemaphoreTest.java
@@ -6,30 +6,30 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 public class SemaphoreTest {
-	private static class Worker {
+	private static class SimpleTask {
 		private Semaphore semaphore = new Semaphore(3);
 
-		public void downloadData() {
+		public void task() {
 			try {
 				semaphore.acquire();
-				System.out.println("Downloading acquired by Thread - " + Thread.currentThread().getName());
+				System.out.println("Semaphore acquire by Thread - " + Thread.currentThread().getName());
 
 				TimeUnit.SECONDS.sleep(3);
 			} catch (InterruptedException ex) {
 				ex.printStackTrace();
 			} finally {
 				semaphore.release();
+				System.out.println("Semaphore release by Thread - " + Thread.currentThread().getName());
 			}
-
-			System.out.println("Downloading released by Thread - " + Thread.currentThread().getName());
 		}
 	}
+
 	public static void main(String[] args) {
-		Worker worker = new Worker();
+		SimpleTask task = new SimpleTask();
 		ExecutorService service = Executors.newCachedThreadPool();
 
 		for(int i=0; i<10; i++)
-			service.execute(() -> worker.downloadData());
+			service.execute(() -> task.task());
 
 		service.shutdown();
 	}
