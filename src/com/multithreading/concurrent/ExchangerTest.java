8c1a20a03ed2fda834288faea6f256b725e71816
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-03-28 10:03:51 +0530

    Exchanger Implementation

--- src/com/multithreading/concurrent/ExchangerTest.java
+++ src/com/multithreading/concurrent/ExchangerTest.java
@@ -1,36 +1,43 @@
 package com.multithreading.concurrent;
 
 import java.util.concurrent.Exchanger;
-
-class ExchangerRunnable extends Thread {
-	private Exchanger<String> exchanger;
-	private String value;
-
-	public ExchangerRunnable(Exchanger<String> exchanger, String value) {
-		this.exchanger = exchanger;
-		this.value = value;
-	}
-
-	@Override
-	public void run() {
-		try {
-			String prevValue = value;
-			String nextValue = exchanger.exchange(prevValue);
-			System.out.println(Thread.currentThread().getName()+" Exchanged "+prevValue +" - "+nextValue);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
-}
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class ExchangerTest {
-	public static void main(String[] args) {
+	private static class SimpleTask implements Runnable {
+		private Exchanger<String> exchanger;
+		private String value;
+
+		public SimpleTask(Exchanger<String> exchanger, String value) {
+			this.exchanger = exchanger;
+			this.value = value;
+		}
+
+		@Override
+		public void run() {
+			try {
+				System.out.println("Thread - " + Thread.currentThread().getName() + " - Previous Value - " + value);
+
+				String newValue = exchanger.exchange(value);
+				System.out.println("Thread - " + Thread.currentThread().getName() + " - New Value - " + newValue);
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		ExecutorService service = Executors.newCachedThreadPool();
 		Exchanger<String> exchanger = new Exchanger<>();
-		ExchangerRunnable runnable1 = new ExchangerRunnable(exchanger, "A");
-		ExchangerRunnable runnable2 = new ExchangerRunnable(exchanger, "B");
-		//ExchangerRunnable runnable3 = new ExchangerRunnable(exchanger, "C");
-		runnable1.start();
-		runnable2.start();
-		//runnable3.start();
+
+		service.execute(new SimpleTask(exchanger, "A"));
+
+		TimeUnit.SECONDS.sleep(3);
+
+		service.execute(new SimpleTask(exchanger, "B"));
+
+		service.shutdown();
 	}
 }
