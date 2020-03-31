ce0bf3a3a3171200e328346fd95f2b94324330fd
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-03-31 18:20:54 +0530

    Terminating Thread by interrupt Method

--- src/com/multithreading/core/TerminatingThreadDemo2.java
+++ src/com/multithreading/core/TerminatingThreadDemo2.java
@@ -4,58 +4,57 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-
-class TerminatingThreadTask2 implements Runnable {
-	@Override
-	public void run() {
-		for(int i=1;; i++) {
-			System.out.println(Thread.currentThread().getName()+" - Calling doAnotherTask().");
-			doAnotherTask();
-
-			if(Thread.interrupted()) {
-				System.out.println(Thread.currentThread().getName()+" - is Interrupted Now.");
-				break;
-			}
-		}
-
-		System.out.println(Thread.currentThread().getName()+" - is Done.");
-	}
-
-	public void doAnotherTask() {
-		List<Integer> intList = new ArrayList<>();
-		Random randomGenerator = new Random();
-		for(int i=0; i<100000; i++)
-			intList.add(randomGenerator.nextInt(100000));
-		Collections.sort(intList);
-	}
-}
+import java.util.concurrent.TimeUnit;
 
 public class TerminatingThreadDemo2 {
-	public static void main(String[] args) {
-		TerminatingThreadTask2 task1 = new TerminatingThreadTask2();
-		TerminatingThreadTask2 task2 = new TerminatingThreadTask2();
-		TerminatingThreadTask2 task3 = new TerminatingThreadTask2();
+	private static class SimpleTask extends Thread {
 
-		Thread thread1 = new Thread(task1, "MyThread-1");
-		Thread thread2 = new Thread(task2, "MyThread-2");
-		Thread thread3 = new Thread(task3, "MyThread-3");
-		thread1.start();
-		thread2.start();
-		thread3.start();
-
-		try {
-			Thread.sleep(3000);
-		} catch (Exception ex) {
-			ex.printStackTrace();
+		public SimpleTask(String name) {
+			super(name);
 		}
 
-		System.out.println("Interrupting Thread MyThread-1");
-		thread1.interrupt();
+		@Override
+		public void run() {
+			for(int i=1;; i++) {
+				System.out.println(Thread.currentThread().getName() + " - Calling doAnotherTask().");
+				doAnotherTask();
 
-		System.out.println("Interrupting Thread MyThread-2");
-		thread2.interrupt();
+				if(Thread.interrupted()) {
+					System.out.println(Thread.currentThread().getName() + " - is Interrupted Now.");
+					break;
+				}
+			}
 
-		System.out.println("Interrupting Thread MyThread-3");
-		thread3.interrupt();
+			System.out.println(Thread.currentThread().getName() + " - is Done.");
+		}
+
+		public void doAnotherTask() {
+			List<Integer> intList = new ArrayList<>();
+			Random randomGenerator = new Random();
+			for(int i=0; i<100000; i++)
+				intList.add(randomGenerator.nextInt(100000));
+			Collections.sort(intList);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		SimpleTask task1 = new SimpleTask("Thread-1");
+		SimpleTask task2 = new SimpleTask("Thread-2");
+		SimpleTask task3 = new SimpleTask("Thread-3");
+
+		task1.start();
+		task2.start();
+		task3.start();
+
+		TimeUnit.SECONDS.sleep(5);
+
+		System.out.println("Interrupting Thread-1");
+		task1.interrupt();
+
+		System.out.println("Interrupting Thread-2");
+		task2.interrupt();
+
+		System.out.println("Interrupting Thread-3");
+		task3.interrupt();
 	}
 }
