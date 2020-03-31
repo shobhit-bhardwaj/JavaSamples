da52956eac02c3efa3e5b010141e72c29d0ff665
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-03-31 09:08:10 +0530

    Notify and NotifyAll Difference

--- src/com/multithreading/core/NotifyAndNotifyAllDemo.java
+++ src/com/multithreading/core/NotifyAndNotifyAllDemo.java
@@ -2,39 +2,39 @@ package com.multithreading.core;
 
 import java.util.concurrent.TimeUnit;
 
-class WaitNotifyTask {
-	public synchronized void waitMethod() {
-		try {
-			System.out.println(Thread.currentThread().getName()+" - waitMethod Start.");
-			wait();
-			System.out.println(Thread.currentThread().getName()+" - waitMethod End.");
-		} catch (Exception ex) {
-			ex.printStackTrace();
+public class NotifyAndNotifyAllDemo {
+	private static class SimpleTask {
+		public synchronized void waitMethod() {
+			try {
+				System.out.println(Thread.currentThread().getName()+" - waitMethod Start.");
+				wait();
+				System.out.println(Thread.currentThread().getName()+" - waitMethod End.");
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		public synchronized void notifyMethod() {
+			notify();
+			System.out.println(Thread.currentThread().getName()+" - notifyMethod Called.");
+		}
+
+		public synchronized void notifyAllMethod() {
+			notifyAll();
+			System.out.println(Thread.currentThread().getName()+" - notifyAllMethod Called.");
 		}
 	}
 
-	public synchronized void notifyMethod() {
-		notify();
-		System.out.println(Thread.currentThread().getName()+" - notifyMethod Called.");
-	}
-
-	public synchronized void notifyAllMethod() {
-		notifyAll();
-		System.out.println(Thread.currentThread().getName()+" - notifyAllMethod Called.");
-	}
-}
-
-public class NotifyAndNotifyAllDemo {
 	public static void main(String[] args) throws Exception {
-		WaitNotifyTask task = new WaitNotifyTask();
+		SimpleTask task = new SimpleTask();
 
-		new Thread(() -> { task.waitMethod(); }, "Thread 1").start();
-		new Thread(() -> { task.waitMethod(); }, "Thread 2").start();
-		new Thread(() -> { task.waitMethod(); }, "Thread 3").start();
+		new Thread(() -> task.waitMethod(), "Thread 1").start();
+		new Thread(() -> task.waitMethod(), "Thread 2").start();
+		new Thread(() -> task.waitMethod(), "Thread 3").start();
 
 		TimeUnit.SECONDS.sleep(1);
 
-		new Thread(() -> { task.notifyMethod(); }, "Thread 4").start();
-		//new Thread(() -> { task.notifyAllMethod(); }, "Thread 4").start();
+		new Thread(() -> task.notifyMethod(), "Thread 4").start();
+		//new Thread(() -> task.notifyAllMethod(), "Thread 4").start();
 	}
 }
