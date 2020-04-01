0131b1a6f9d0a3fe3b33ec2d6c29ad4c13e0cc8f
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-04-01 11:36:45 +0530

    SingleThreadExecutor Implementation

--- src/com/multithreading/concurrent/executorFramework/SingleThreadExecutorTest.java
+++ src/com/multithreading/concurrent/executorFramework/SingleThreadExecutorTest.java
@@ -4,14 +4,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class SingleThreadExecutorDemo {
-	private static class SimpleThread implements Runnable {
+public class SingleThreadExecutorTest {
+	private static class SimpleTask implements Runnable {
 		@Override
 		public void run() {
 			try {
 				for(int i=0; i<5; i++) {
 					TimeUnit.SECONDS.sleep(1);
-					System.out.println("Counter - " + (i + 1) + " - By Thread - " + Thread.currentThread().getName());
+					System.out.println("Counter - " + (i + 1) + " - by Thread - " + Thread.currentThread().getName());
 				}
 			} catch (InterruptedException ex) {
 				ex.printStackTrace();
@@ -22,9 +22,9 @@ public class SingleThreadExecutorDemo {
 	public static void main(String[] args) {
 		ExecutorService executor = Executors.newSingleThreadExecutor();
 
-		executor.submit(new SimpleThread());
-		executor.submit(new SimpleThread());
-		executor.submit(new SimpleThread());
+		executor.submit(new SimpleTask());
+		executor.submit(new SimpleTask());
+		executor.submit(new SimpleTask());
 
 		executor.shutdown();
 	}
