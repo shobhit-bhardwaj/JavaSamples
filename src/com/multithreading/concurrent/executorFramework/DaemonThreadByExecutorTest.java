393471a604d5ab965ff45ef897044b6331288cb8
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-04-01 11:31:47 +0530

    Daemon Thread Creation in Executor

--- src/com/multithreading/concurrent/executorFramework/DaemonThreadByExecutorTest.java
+++ src/com/multithreading/concurrent/executorFramework/DaemonThreadByExecutorTest.java
@@ -5,7 +5,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
-public class DaemonThreadByExecutorDemo {
+public class DaemonThreadByExecutorTest {
 	private static class MyThreadFactory implements ThreadFactory {
 		private static int counter = 0;
 
@@ -24,10 +24,10 @@ public class DaemonThreadByExecutorDemo {
 		}
 	}
 
-	private static class SimpleThread implements Runnable {
+	private static class SimpleTask implements Runnable {
 		private int delay;
 
-		public SimpleThread(int delay) {
+		public SimpleTask(int delay) {
 			this.delay = delay;
 		}
 
@@ -46,8 +46,8 @@ public class DaemonThreadByExecutorDemo {
 	public static void main(String[] args) {
 		ExecutorService service = Executors.newCachedThreadPool(new MyThreadFactory());
 
-		service.execute(new SimpleThread(5));
-		service.execute(new SimpleThread(2));
+		service.execute(new SimpleTask(5));
+		service.execute(new SimpleTask(2));
 
 		service.shutdown();
 	}
