15283a524df78e416e67ec7c2148d4042ddf27b8
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-04-06 19:32:47 +0530

    Remove Single Element is List

--- src/com/collection/problems/RemoveParticularSingleElementList.java
+++ src/com/collection/problems/RemoveParticularSingleElementList.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class RemoveParticularSameElementList {
+public class RemoveParticularSingleElementList {
 	public static void main(String[] args) {
 		List<Integer> integers = new ArrayList<>(Arrays.asList(1, null, 2, null, 3, 3, 4, 5, 5, 6, 7));
+		System.out.println("Initial List - " + integers);
+
 		Integer removeElement = null;
 		//Integer removeElement = 3;
-		System.out.println("Initial List - " + integers);
 		System.out.println("Remove Element - " + removeElement);
 
 		while(integers.remove(removeElement));
