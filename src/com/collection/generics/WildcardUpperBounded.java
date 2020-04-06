3b88c488a727ced8ee673933be9a9653ac816b8e
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-04-06 18:09:47 +0530

    Wildcard Upper Bounded

---
+++ src/com/collection/generics/WildcardUpperBounded.java
@@ -0,0 +1,19 @@
+package com.collection.generics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WildcardUpperBounded {
+	public static void main(String[] args) {
+		List<Integer> integers = new ArrayList<>(Arrays.asList(1, 2, 3));
+		printList(integers);
+
+		List<Double> doubles = new ArrayList<>(Arrays.asList(1.0, 2.0, 3.0));
+		printList(doubles);
+	}
+
+	public static void printList(List<? extends Number> list) {
+		System.out.println(list);
+	}
+}
