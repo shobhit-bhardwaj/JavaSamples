0bf69e6b067aec33fba6ac161f6b88aee6b6ca2d
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-04-13 17:01:28 +0530

    Java Design Patterns - Prototype Pattern

--- src/com/designPattern/creational/prototype/Client.java
+++ src/com/designPattern/creational/prototype/Client.java
@@ -2,6 +2,12 @@ package com.designPattern.creational.prototype;
 
 import java.util.List;
 
+/*
+ * In Prototype Design Pattern, an instance of actual object (i.e. prototype) is created on starting
+ * and thereafter whenever a new instance is required, this prototype is cloned to have another instance.
+ * 
+ * The main advantage of this pattern is to have minimal instance creation process which is much costly than cloning process.
+ */
 public class Client {
 	public static void main(String[] args) {
 		try {
