6597da91bae62b4bbda5421d8cc826fc5f0a6ea3
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-04-16 12:28:05 +0530

    Java Design Patterns - Chain of Responsibility Pattern

--- src/com/designPattern/behavioral/chainOfResponsibility/Client.java
+++ src/com/designPattern/behavioral/chainOfResponsibility/Client.java
@@ -1,6 +1,12 @@
 package com.designPattern.behavioral.chainOfResponsibility;
 
-public class TestChainOfResponsibility {
+/*
+ * The Chain of Responsibility Design Pattern gives more than one object an opportunity to handle a request by linking receiving objects together.
+ * It allows a number of classes to attempt to handle a request, independently of any other object along the chain.
+ * 
+ * Once the request is handled, it completes it’s journey through the chain. Extra handlers can be added or removed from chain without modifying the logic inside any of concrete handler.
+ */
+public class Client {
 	public static void main(String[] args) {
 		DispenseChain chain1 = new Dollar50Dispenser();
 		DispenseChain chain2 = new Dollar20Dispenser();
