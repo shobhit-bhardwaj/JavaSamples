917e51917b6dc8c5f5e81abb7e7dd0f79757f4ec
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-04-26 18:49:05 +0530

    Selection Sort

--- src/com/dataStructure/sorting/SelectionSort.java
+++ src/com/dataStructure/sorting/SelectionSort.java
@@ -1,10 +1,15 @@
 package com.dataStructure.sorting;
 
-public class SelectionSortTest {
-	public void sort(int[] array) {
+import java.util.Arrays;
+
+/*
+ * Complexity - O(n2)
+ */
+public class SelectionSort {
+	public static void sort(int[] array) {
 		int length = array.length;
 
-		for(int i=0; i<length; i++) {
+		for(int i=0; i<length-1; i++) {
 			for(int j=i+1; j<length; j++) {
 				if(array[i] > array[j]) {
 					int temp = array[i];
@@ -16,11 +21,10 @@ public class SelectionSortTest {
 	}
 
 	public static void main(String[] args) {
-		SelectionSortTest sort = new SelectionSortTest();
-
 		int[] array = new int[]{43, 54, 21, 12, 58, 76, 38, 26, 15, 66};
-		sort.sort(array);
-		for(int value : array)
-			System.out.print(value + "\t");
+
+		sort(array);
+
+		System.out.println(Arrays.toString(array));
 	}
 }
