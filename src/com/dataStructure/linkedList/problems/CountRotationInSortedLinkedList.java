ff891dd201a1a71e4a2326a828ffb662db3831a9
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-05-03 13:15:44 +0530

    Count Rotation in Sorted and Rotated Linked List

--- src/com/dataStructure/linkedList/problems/CountRotationInSortedLinkedList.java
+++ src/com/dataStructure/linkedList/problems/CountRotationInSortedLinkedList.java
@@ -1,9 +1,7 @@
 package com.dataStructure.linkedList.problems;
 
-import com.dataStructure.linkedList.problems.LinkedListDemo.Node;
-
 public class CountRotationInSortedLinkedList {
-	public Node countRotation(Node head) {
+	private static Node countRotation(Node head) {
 		Node node = head;
 
 		int k = 1;
@@ -16,7 +14,7 @@ public class CountRotationInSortedLinkedList {
 			node = node.next;
 		}
 
-		System.out.println("Rotation No - "+k);
+		System.out.println("Rotation No - " + k);
 
 		node.next = head;
 		head = newHead.next;
@@ -26,17 +24,17 @@ public class CountRotationInSortedLinkedList {
 	}
 
 	public static void main(String[] args) {
-		LinkedListDemo linkedList = new LinkedListDemo();
-		linkedList.insertData(13);
-		linkedList.insertData(18);
-		linkedList.insertData(5);
-		linkedList.insertData(9);
-		linkedList.insertData(11);
-		Node head = linkedList.getHead();
-		linkedList.traverse(head);
+		SinglyLinkedList linkedList = new SinglyLinkedList();
+		Node head = linkedList
+				.insertData(13)
+				.insertData(18)
+				.insertData(5)
+				.insertData(9)
+				.insertData(11)
+				.getHead();
+		System.out.println(linkedList);
 
-		CountRotationInSortedLinkedList list = new CountRotationInSortedLinkedList();
-		head = list.countRotation(head);
-		linkedList.traverse(head);
+		head = countRotation(head);
+		SinglyLinkedList.traverse(head);
 	}
 }
