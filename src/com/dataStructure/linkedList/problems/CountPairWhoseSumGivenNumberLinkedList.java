764fd3db162e5b7dbac0c3ebcfc4fe92e915fd88
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-05-03 13:15:23 +0530

    Count Pairs From Two Linked Lists Whose Sum is Equal to a Given Value

--- src/com/dataStructure/linkedList/problems/CountPairWhoseSumGivenNumberLinkedList.java
+++ src/com/dataStructure/linkedList/problems/CountPairWhoseSumGivenNumberLinkedList.java
@@ -1,9 +1,7 @@
 package com.dataStructure.linkedList.problems;
 
-import com.dataStructure.linkedList.problems.LinkedListDemo.Node;
-
 public class CountPairWhoseSumGivenNumberLinkedList {
-	public int countPair(Node head1, Node head2, int sum) {
+	private static int countPair(Node head1, Node head2, int sum) {
 		int count = 0;
 
 		Node first = head1;
@@ -22,30 +20,31 @@ public class CountPairWhoseSumGivenNumberLinkedList {
 	}
 
 	public static void main(String[] args) {
-		LinkedListDemo linkedList = new LinkedListDemo();
-		linkedList.insertData(4);
-		linkedList.insertData(3);
-		linkedList.insertData(5);
-		linkedList.insertData(7);
-		linkedList.insertData(11);
-		linkedList.insertData(2);
-		linkedList.insertData(1);
-		Node head1 = linkedList.getHead();
-		linkedList.traverse(head1);
+		SinglyLinkedList linkedList = new SinglyLinkedList();
+		Node head1 = linkedList
+				.insertData(4)
+				.insertData(3)
+				.insertData(5)
+				.insertData(7)
+				.insertData(11)
+				.insertData(2)
+				.insertData(1)
+				.getHead();
+		System.out.println(linkedList);
 
-		linkedList = new LinkedListDemo();
-		linkedList.insertData(2);
-		linkedList.insertData(3);
-		linkedList.insertData(4);
-		linkedList.insertData(5);
-		linkedList.insertData(6);
-		linkedList.insertData(8);
-		linkedList.insertData(12);
-		Node head2 = linkedList.getHead();
-		linkedList.traverse(head2);
+		linkedList = new SinglyLinkedList();
+		Node head2 = linkedList
+				.insertData(2)
+				.insertData(3)
+				.insertData(4)
+				.insertData(5)
+				.insertData(6)
+				.insertData(8)
+				.insertData(12)
+				.getHead();
+		System.out.println(linkedList);
 
-		CountPairWhoseSumGivenNumberLinkedList list = new CountPairWhoseSumGivenNumberLinkedList();
-		int count = list.countPair(head1, head2, 9);
-		System.out.println("Count is - "+count);
+		int count = countPair(head1, head2, 9);
+		System.out.println("Count is - " + count);
 	}
 }
