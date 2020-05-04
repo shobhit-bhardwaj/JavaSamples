54ab4f83e4e8a520c991279ad1331221856af90d
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-05-04 09:21:23 +0530

    Loop in Linked List

--- src/com/dataStructure/linkedList/problems/LoopInLinkedList.java
+++ src/com/dataStructure/linkedList/problems/LoopInLinkedList.java
@@ -1,41 +1,117 @@
 package com.dataStructure.linkedList.problems;
 
-import com.dataStructure.linkedList.problems.LinkedListDemo.Node;
-
 public class LoopInLinkedList {
-	public boolean floydCycleAlgorithm(Node node) {
-		Node temp1 = node;
-		Node temp2 = node;
-		while(temp2 != null && temp2.next != null) {
-			temp2 = temp2.next.next;
-			temp1 = temp1.next;
+	private static Node createLoopInLinkedList(Node head) {
+		Node node = head;
+		Node middle = head;
+		while(node.next != null) {
+			if(node.data == 40) {
+				middle = node;
+			}
 
-			if(temp1 == temp2)
+			node= node.next;
+		}
+		node.next = middle;
+
+		return head;
+	}
+
+	private static boolean floydCycleAlgorithm(Node head) {
+		Node slow = head;
+		Node fast = head;
+		while(fast.next != null && fast.next.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+
+			if(slow == fast)
 				return true;
 		}
 
 		return false;
 	}
 
-	public void test() {
-		LinkedListDemo linkedList = new LinkedListDemo();
-		Node head = linkedList.createNode(10);
-		Node node1 = linkedList.createNode(20);
-		Node node2 = linkedList.createNode(30);
-		Node node3 = linkedList.createNode(40);
-		Node node4 = linkedList.createNode(50);
-		head.next = node1;
-		head.next.next = node2;
-		head.next.next.next = node3;
-		head.next.next.next.next = node4;
-		head.next.next.next.next.next = node1;
+	private static int lengthOfLoopInLinkedList(Node head) {
+		boolean loopFound = false;
 
-		LoopInLinkedList loopDetection = new LoopInLinkedList();
-		boolean loopFound = loopDetection.floydCycleAlgorithm(head);
-		System.out.println("Loop Found - "+loopFound);
+		Node slow = head;
+		Node fast = head;
+		while(fast.next != null && fast.next.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+
+			if(slow == fast) {
+				loopFound = true;
+				break;
+			}
+		}
+
+		int length = 0;
+		if(loopFound) {
+			fast = fast.next;
+
+			while(slow != fast) {
+				if(slow == fast)
+					return length;
+
+				fast = fast.next;
+				length++;
+			}
+		}
+
+		return length;
+	}
+
+	private static Node removeLoopInLinkedList(Node head) {
+		boolean loopFound = false;
+
+		Node slow = head;
+		Node fast = head;
+		while(fast.next != null && fast.next.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+
+			if(slow == fast) {
+				loopFound = true;
+				break;
+			}
+		}
+
+		if(loopFound) {
+			slow = head;
+			while(fast.next != slow.next) {
+				slow = slow.next;
+				fast = fast.next;
+			}
+
+			fast.next = null;
+		}
+
+		return head;
 	}
 
 	public static void main(String[] args) {
-		new LoopInLinkedList().test();
+		SinglyLinkedList linkedList = new SinglyLinkedList();
+		Node head = linkedList.insertData(10)
+				.insertData(20)
+				.insertData(30)
+				.insertData(40)
+				.insertData(50)
+				.insertData(60)
+				.insertData(70)
+				.insertData(80)
+				.insertData(90)
+				.getHead();
+		System.out.println(linkedList);
+
+		head = createLoopInLinkedList(head);
+
+		boolean loop = floydCycleAlgorithm(head);
+		System.out.println("Loop Found - " + loop);
+
+		int length = lengthOfLoopInLinkedList(head);
+		System.out.println("Length of Loop - " + length);
+
+		head = removeLoopInLinkedList(head);
+		SinglyLinkedList.traverse(head);
 	}
 }
