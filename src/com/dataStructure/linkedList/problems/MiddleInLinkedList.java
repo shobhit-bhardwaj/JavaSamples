ec75910babd1aa1318885a8e39f3f44b6e4eae5a
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-05-04 09:23:58 +0530

    Middle in Linked List

--- src/com/dataStructure/linkedList/problems/MiddleInLinkedList.java
+++ src/com/dataStructure/linkedList/problems/MiddleInLinkedList.java
@@ -1,31 +1,29 @@
 package com.dataStructure.linkedList.problems;
 
-import com.dataStructure.linkedList.problems.LinkedListDemo.Node;
-
 public class MiddleInLinkedList {
-	public Node middleNode(Node node) {
-		Node temp1 = node;
-		Node temp2 = node;
-		while(temp2 != null && temp2.next != null) {
-			temp2 = temp2.next.next;
-			temp1 = temp1.next;
+	private static Node middleNode(Node node) {
+		Node slow = node;
+		Node fast = node;
+		while(fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
 		}
 
-		return temp1;
+		return slow;
 	}
 
 	public static void main(String[] args) {
-		LinkedListDemo linkedList = new LinkedListDemo();
-		linkedList.insertData(10);
-		linkedList.insertData(20);
-		linkedList.insertData(30);
-		linkedList.insertData(40);
-		linkedList.insertData(50);
+		SinglyLinkedList linkedList = new SinglyLinkedList();
+		Node head = linkedList.insertData(10)
+				.insertData(20)
+				.insertData(30)
+				.insertData(40)
+				.insertData(50)
+				.insertData(60)
+				.getHead();
+		System.out.println(linkedList);
 
-		Node head = linkedList.getHead();
-
-		MiddleInLinkedList middle = new MiddleInLinkedList();
-		Node node = middle.middleNode(head);
-		System.out.println("Middle Node - "+node);
+		Node node = middleNode(head);
+		System.out.println("Middle Node - " + node.getData());
 	}
 }
