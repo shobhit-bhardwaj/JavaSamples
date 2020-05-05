403493898e0ac11c126e69fea8fcc60b4cb550ad
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-05-05 10:08:43 +0530

    Rearrange Linked List in Place

--- src/com/dataStructure/linkedList/problems/RearrangeLinkedListInPlace.java
+++ src/com/dataStructure/linkedList/problems/RearrangeLinkedListInPlace.java
@@ -1,9 +1,7 @@
 package com.dataStructure.linkedList.problems;
 
-import com.dataStructure.linkedList.problems.LinkedListDemo.Node;
-
 public class RearrangeLinkedListInPlace {
-	public Node rearrange(Node head) {
+	private static Node rearrange(Node head) {
 		Node node = head;
 		while(node.next != null) {
 			Node second = node;
@@ -20,17 +18,16 @@ public class RearrangeLinkedListInPlace {
 	}
 
 	public static void main(String[] args) {
-		LinkedListDemo linkedList = new LinkedListDemo();
-		linkedList.insertData(10);
-		linkedList.insertData(20);
-		linkedList.insertData(30);
-		linkedList.insertData(40);
-		linkedList.insertData(50);
-		Node head = linkedList.getHead();
-		linkedList.traverse(head);
+		SinglyLinkedList linkedList = new SinglyLinkedList();
+		Node head = linkedList.insertData(10)
+				.insertData(20)
+				.insertData(30)
+				.insertData(40)
+				.insertData(50)
+				.getHead();
+		System.out.println(linkedList);
 
-		RearrangeLinkedListInPlace rearrangeLinkedList = new RearrangeLinkedListInPlace();
-		head = rearrangeLinkedList.rearrange(head);
-		linkedList.traverse(head);
+		head = rearrange(head);
+		SinglyLinkedList.traverse(head);
 	}
 }
