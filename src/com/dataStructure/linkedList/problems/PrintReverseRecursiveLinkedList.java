84290eeae765bd85e0588c7e970a1ad2f35c8cdd
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-05-05 10:08:21 +0530

    Print Reverse Recursive Linked List

--- src/com/dataStructure/linkedList/problems/PrintReverseRecursiveLinkedList.java
+++ src/com/dataStructure/linkedList/problems/PrintReverseRecursiveLinkedList.java
@@ -1,27 +1,25 @@
 package com.dataStructure.linkedList.problems;
 
-import com.dataStructure.linkedList.problems.LinkedListDemo.Node;
-
 public class PrintReverseRecursiveLinkedList {
-	public void printReverseRecursive(Node node) {
-		if(node == null)
+	private static void printReverseRecursive(Node node) {
+		if(node == null) {
 			return;
-		else {
+		} else {
 			printReverseRecursive(node.next);
-			System.out.print(node.data+"\t");
+			System.out.print(node.data + "  -->  ");
 		}
 	}
 
 	public static void main(String[] args) {
-		LinkedListDemo linkedList = new LinkedListDemo();
-		linkedList.insertData(10);
-		linkedList.insertData(20);
-		linkedList.insertData(30);
-		linkedList.insertData(40);
-		linkedList.insertData(50);
+		SinglyLinkedList linkedList = new SinglyLinkedList();
+		Node head = linkedList.insertData(10)
+				.insertData(20)
+				.insertData(30)
+				.insertData(40)
+				.insertData(50)
+				.getHead();
+		System.out.println(linkedList);
 
-		Node head = linkedList.getHead();
-		PrintReverseRecursiveLinkedList printReverse = new PrintReverseRecursiveLinkedList();
-		printReverse.printReverseRecursive(head);
+		printReverseRecursive(head);
 	}
 }
