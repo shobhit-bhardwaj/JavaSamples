6a75f19a3124ebb29971208e8217c6e92779a49d
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-05-06 11:30:56 +0530

    Segregate Even Odd Node in Linked List

--- src/com/dataStructure/linkedList/problems/SegregateEvenOddNodeLinkedList.java
+++ src/com/dataStructure/linkedList/problems/SegregateEvenOddNodeLinkedList.java
@@ -1,21 +1,19 @@
 package com.dataStructure.linkedList.problems;
 
-import com.dataStructure.linkedList.problems.LinkedListDemo.Node;
-
 public class SegregateEvenOddNodeLinkedList {
-	public Node segregateEvenOdd(Node head) {
+	private static Node segregateEvenOdd(Node head) {
 		Node odd = null;
 		Node current = head;
 		Node prev = null;
 		while(current != null) {
 			if(current.data%2 == 1) {
 				if(odd == null) {
-					odd = new LinkedListDemo().createNode(current.data);
+					odd = Node.createNode(current.data);
 				} else {
 					Node temp = odd;
 					while(temp.next != null)
 						temp = temp.next;
-					temp.next = new LinkedListDemo().createNode(current.data);
+					temp.next = Node.createNode(current.data);
 				}
 				if(prev == null)
 					head = head.next;
@@ -35,19 +33,19 @@ public class SegregateEvenOddNodeLinkedList {
 	}
 
 	public static void main(String[] args) {
-		LinkedListDemo linkedList = new LinkedListDemo();
-		linkedList.insertData(4);
-		linkedList.insertData(1);
-		linkedList.insertData(2);
-		linkedList.insertData(7);
-		linkedList.insertData(8);
-		linkedList.insertData(12);
-		linkedList.insertData(11);
-		linkedList.insertData(14);
-		Node head = linkedList.getHead();
+		SinglyLinkedList linkedList = new SinglyLinkedList();
+		Node head = linkedList.insertData(4)
+				.insertData(1)
+				.insertData(2)
+				.insertData(7)
+				.insertData(8)
+				.insertData(12)
+				.insertData(11)
+				.insertData(14)
+				.getHead();
+		System.out.println(linkedList);
 
-		SegregateEvenOddNodeLinkedList segregate = new SegregateEvenOddNodeLinkedList();
-		head = segregate.segregateEvenOdd(head);
-		linkedList.traverse(head);
+		head = segregateEvenOdd(head);
+		SinglyLinkedList.traverse(head);
 	}
 }
