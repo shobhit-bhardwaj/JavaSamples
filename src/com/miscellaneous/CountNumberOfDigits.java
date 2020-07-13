9957f73accd9980adc051c3ae342ec66c8b6e3fb
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-07-13 15:11:41 +0530

    Delete Moved Resources

--- src/com/miscellaneous/CountNumberOfDigits.java
+++
@@ -1,9 +0,0 @@
-package com.miscellaneous;
-
-public class CountNumberOfDigits {
-	public static void main(String[] args) {
-		int number = 10_00_000;
-		int digits = (int) Math.floor(Math.log10(number)) + 1;
-		System.out.println("Number of Digits in "+number+" is - "+digits);
-	}
-}
