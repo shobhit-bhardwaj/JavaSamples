b586489591f857656bd51c0251b9c7580c9a43c8
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-07-13 15:14:12 +0530

    Keep the System Awake with Mouse Hover

---
+++ src/com/javaBasics/KeepSystemAwake.java
@@ -0,0 +1,17 @@
+package com.javaBasics;
+
+import java.awt.Robot;
+import java.util.Random;
+
+public class KeepSystemAwake {
+	public static void main(String[] args) throws Exception {
+		Robot robot = new Robot();
+		Random random = new Random();
+		while (true) {
+			robot.delay(1000 * 60);
+			int x = random.nextInt() % 640;
+			int y = random.nextInt() % 480;
+			robot.mouseMove(x, y);
+		}
+	}
+}
