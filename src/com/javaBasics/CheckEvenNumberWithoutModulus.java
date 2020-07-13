cd24b66aa004616c2bda504524ac0ed7211a88fa
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-07-13 15:12:58 +0530

    Check Even Number Without Modulus

---
+++ src/com/javaBasics/CheckEvenNumberWithoutModulus.java
@@ -0,0 +1,13 @@
+package com.javaBasics;
+
+import java.util.stream.IntStream;
+
+public class CheckEvenNumberWithoutModulus {
+	public static void main(String[] args) {
+		int number = 4;
+		System.out.println(number +" is even - "+ ((number&1) == 0));
+		System.out.println();
+
+		IntStream.range(0, 25).filter(n -> (n&1) == 0).forEach(n -> System.out.println(n +" is even"));
+	}
+}
