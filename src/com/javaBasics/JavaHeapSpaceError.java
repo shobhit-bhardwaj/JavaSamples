110804451fbbd19784b42608ae6dcebb043a8952
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-07-13 15:13:48 +0530

    Create Java Heap Space Error

---
+++ src/com/javaBasics/JavaHeapSpaceError.java
@@ -0,0 +1,15 @@
+package com.javaBasics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaHeapSpaceError {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+		while(true) {
+			list.add("Hello World");
+		}
+	}
+}
+
+//	VM Argument Passed in Run Configuration - -Xmx20m
