ce9df4ff29a1e1ab8587ae52558936ea58cf4779
Shobhit Bhardwaj <successshobhit@gmail.com>
2020-10-06 19:45:14 +0530

    Java 8 Streams

--- src/com/java8/Java8Streams.java
+++ src/com/java8/Java8Streams.java
@@ -12,38 +12,41 @@ public class Java8Streams {
 		final List<Integer> integerList = Arrays.asList(1, 2, 3, 2, 4, 3, 5, 6, 7);
 
 		long count = stringList.stream().filter(s -> s.isEmpty()).count();
-		System.out.println("Empty String Count - "+count);
+		System.out.println("Empty String Count - " + count);
 
 		count = stringList.parallelStream().filter(s -> !s.isEmpty()).count();
-		System.out.println("UnEmpty String Count - "+count);
+		System.out.println("UnEmpty String Count - " + count);
 
-		count = stringList.stream().filter(s -> s.length()==3).count();
-		System.out.println("Length 3 String Count - "+count);
+		count = stringList.stream().filter(s -> s.length() == 3).count();
+		System.out.println("Length 3 String Count - " + count);
 
 		List<String> filteredList = stringList.stream().filter(s -> !s.isEmpty()).collect(Collectors.toList());
-		System.out.println("Filtered List - "+filteredList);
+		System.out.println("Filtered List - " + filteredList);
 
 		String mergeString = stringList.stream().filter(s -> !s.isEmpty()).collect(Collectors.joining(", "));
-		System.out.println("Merge String - "+mergeString);
+		System.out.println("Merge String - " + mergeString);
 
-		List<Integer> squareList = integerList.stream().map(i -> i*i).distinct().collect(Collectors.toList());
-		System.out.println("Square List - "+squareList);
+		List<Integer> squareList = integerList.stream().map(i -> i * i).distinct().collect(Collectors.toList());
+		System.out.println("Square List - " + squareList);
 
 		String str = stringList.stream().min(Comparator.comparing(a -> a.length())).get();
-		System.out.println("Min - "+str);
+		System.out.println("Min - " + str);
 		str = stringList.stream().max(Comparator.comparing(a -> a.length())).get();
-		System.out.println("Max - "+str);
+		System.out.println("Max - " + str);
 
-		String reduce = stringList.stream().reduce("Init", (acc, item) -> {System.out.println(acc+"|"+item); return acc+" "+item;});
-		System.out.println("Reduce - "+reduce);
+		String reduce = stringList.stream().reduce("Init", (acc, item) -> {
+			System.out.println(acc + "|" + item);
+			return acc + " " + item;
+		});
+		System.out.println("Reduce - " + reduce);
 
-		//	LongSummaryStatistics
-		//	DoubleSummaryStatistics
+		// LongSummaryStatistics
+		// DoubleSummaryStatistics
 		IntSummaryStatistics statistics = integerList.stream().mapToInt(i -> i).summaryStatistics();
-		System.out.println("Get Min - "+statistics.getMin());
-		System.out.println("Get Max - "+statistics.getMax());
-		System.out.println("Get Count - "+statistics.getCount());
-		System.out.println("Get Sum - "+statistics.getSum());
-		System.out.println("Get Average - "+statistics.getAverage());
+		System.out.println("Get Min - " + statistics.getMin());
+		System.out.println("Get Max - " + statistics.getMax());
+		System.out.println("Get Count - " + statistics.getCount());
+		System.out.println("Get Sum - " + statistics.getSum());
+		System.out.println("Get Average - " + statistics.getAverage());
 	}
 }
