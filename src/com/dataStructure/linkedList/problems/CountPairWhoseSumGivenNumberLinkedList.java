7496b72aa9fd911a55f393abf77e1459de81676c
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-07-20 13:14:40 +0530

    Count Pairs From Two Linked Lists Whose Sum is Equal to a Given Value

--- src/com/dataStructure/linkedList/problems/CountPairWhoseSumGivenNumberLinkedList.java
+++ src/com/dataStructure/linkedList/problems/CountPairWhoseSumGivenNumberLinkedList.java
@@ -4,6 +4,16 @@ public class CountPairWhoseSumGivenNumberLinkedList {
 	private static int countPair(Node head1, Node head2, int sum) {
 		int count = 0;
 
+		//	Using for loop
+		for(Node temp1=head1; temp1!=null;temp1=temp1.next) {
+			for(Node temp2=head2; temp2!=null;temp2=temp2.next) {
+				if(temp1.data + temp2.data == sum)
+					count++;
+			}
+		}
+
+		count = 0;
+		//	Using while loop
 		Node first = head1;
 		while(first != null) {
 			Node second = head2;
@@ -20,8 +30,7 @@ public class CountPairWhoseSumGivenNumberLinkedList {
 	}
 
 	public static void main(String[] args) {
-		SinglyLinkedList linkedList = new SinglyLinkedList();
-		Node head1 = linkedList
+		Node head1 = new SinglyLinkedList()
 				.insertData(4)
 				.insertData(3)
 				.insertData(5)
@@ -30,10 +39,9 @@ public class CountPairWhoseSumGivenNumberLinkedList {
 				.insertData(2)
 				.insertData(1)
 				.getHead();
-		System.out.println(linkedList);
+		SinglyLinkedList.traverse(head1);
 
-		linkedList = new SinglyLinkedList();
-		Node head2 = linkedList
+		Node head2 = new SinglyLinkedList()
 				.insertData(2)
 				.insertData(3)
 				.insertData(4)
@@ -42,7 +50,7 @@ public class CountPairWhoseSumGivenNumberLinkedList {
 				.insertData(8)
 				.insertData(12)
 				.getHead();
-		System.out.println(linkedList);
+		SinglyLinkedList.traverse(head2);
 
 		int count = countPair(head1, head2, 9);
 		System.out.println("Count is - " + count);
