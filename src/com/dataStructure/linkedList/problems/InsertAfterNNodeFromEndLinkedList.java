b69cb6bbc472a08efed59c3d5e158f45bd4866e2
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-07-21 21:22:53 +0530

    Insert After N Node from End Linked List

--- src/com/dataStructure/linkedList/problems/InsertAfterNNodeFromEndLinkedList.java
+++ src/com/dataStructure/linkedList/problems/InsertAfterNNodeFromEndLinkedList.java
@@ -1,37 +1,71 @@
 package com.dataStructure.linkedList.problems;
 
 public class InsertAfterNNodeFromEndLinkedList {
-	private static Node insertAfterNNodeFromEnd(Node head, Node newNode, int n) {
-		int i = 0;
-		Node fastNode = head;
-		while(i != n) {
-			fastNode = fastNode.next;
-			i++;
+
+	/*
+	 * Approach 1 - Find the length and then traverse the list from length - position.
+	 * It will take two traverse but one space.
+	 * 
+	 */
+	private static Node insertAfterNNodeFromEndByLength(Node head, int data, int position) {
+		int length = 0;
+		for(Node temp=head; temp!=null; temp=temp.next)
+			length++;
+
+		position = length - position;
+
+		Node node = Node.createNode(data);
+
+		Node temp = head;
+		for(int i=0; i<position-1; i++)
+			temp = temp.next;
+
+		node.next = temp.next;
+		temp.next = node;
+
+		return head;
+	}
+
+	/*
+	 * Approach 2 - It will take two pointers but only one traverse.
+	 * 
+	 */
+	private static Node insertAfterNNodeFromEndByTwoPointers(Node head, int data, int position) {
+		Node fast = head;
+		for(int i=0; i<position; i++)
+			fast = fast.next;
+
+		Node slow = head;
+		while(fast.next != null) {
+			fast = fast.next;
+			slow = slow.next;
 		}
 
-		Node node = head;
-		while(fastNode != null) {
-			node = node.next;
-			fastNode = fastNode.next;
-		}
+		Node node = Node.createNode(data);
 
-		newNode.next = node.next;
-		node.next = newNode;
+		node.next = slow.next;
+		slow.next = node;
 
 		return head;
 	}
 
 	public static void main(String[] args) {
-		SinglyLinkedList linkedList = new SinglyLinkedList();
-		Node head = linkedList.insertData(10)
+		Node head = new SinglyLinkedList()
+				.insertData(10)
 				.insertData(20)
 				.insertData(30)
 				.insertData(40)
 				.insertData(50)
+				.insertData(60)
+				.insertData(70)
+				.insertData(80)
+				.insertData(90)
+				.insertData(100)
 				.getHead();
-		System.out.println(linkedList);
+		SinglyLinkedList.traverse(head);
 
-		head = insertAfterNNodeFromEnd(head, Node.createNode(25), 3);
+		head = insertAfterNNodeFromEndByLength(head, 75, 3);
+		//head = insertAfterNNodeFromEndByTwoPointers(head, 75, 3);
 		SinglyLinkedList.traverse(head);
 	}
 }
