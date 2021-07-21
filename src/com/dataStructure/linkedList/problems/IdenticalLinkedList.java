6d224110931c896ed0ab0c63e8af47777353df05
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-07-21 15:57:22 +0530

    Identical Linked List

--- src/com/dataStructure/linkedList/problems/IdenticalLinkedList.java
+++ src/com/dataStructure/linkedList/problems/IdenticalLinkedList.java
@@ -5,36 +5,35 @@ public class IdenticalLinkedList {
 		while(head1 != null && head2 != null) {
 			if(head1.data != head2.data)
 				return false;
+
 			head1 = head1.next;
 			head2 = head2.next;
 		}
 
-		if(head1 != null)
-			return false;
-		else if(head2 != null)
+		if(head1 != null || head2 != null)
 			return false;
 
 		return true;
 	}
 
 	public static void main(String[] args) {
-		SinglyLinkedList linkedList = new SinglyLinkedList();
-		Node head1 = linkedList.insertData(10)
+		Node head1 = new SinglyLinkedList()
+				.insertData(10)
 				.insertData(20)
 				.insertData(30)
 				.insertData(40)
 				.insertData(50)
 				.getHead();
-		System.out.println(linkedList);
+		SinglyLinkedList.traverse(head1);
 
-		linkedList = new SinglyLinkedList();
-		Node head2 = linkedList.insertData(10)
+		Node head2 = new SinglyLinkedList()
+				.insertData(10)
 				.insertData(20)
 				.insertData(30)
 				.insertData(40)
 				.insertData(50)
 				.getHead();
-		System.out.println(linkedList);
+		SinglyLinkedList.traverse(head1);
 
 		boolean isIdentical = identical(head1, head2);
 		System.out.println("Is Identical List - " + isIdentical);
