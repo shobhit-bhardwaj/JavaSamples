d891e94280e47df1c0ab08146046b60afda35f9f
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-07-21 14:05:31 +0530

    Delete Alternate Nodes in Linked List

--- src/com/dataStructure/linkedList/problems/DeleteAlternateNodes.java
+++ src/com/dataStructure/linkedList/problems/DeleteAlternateNodes.java
@@ -2,18 +2,25 @@ package com.dataStructure.linkedList.problems;
 
 public class DeleteAlternateNodes {
 	private static Node deleteAlternateNodes(Node head) {
+		//	Using for loop
+		for(Node node=head; (node!=null && node.next!=null); node=node.next) {
+			node.next = node.next.next;
+		}
+
+		//	Using while loop
+		/*
 		Node node = head;
 		while(node != null && node.next != null) {
 			node.next = node.next.next;
 			node = node.next;
-		}
+		}*/
 
 		return head;
 	}
 
 	public static void main(String[] args) {
-		SinglyLinkedList linkedList = new SinglyLinkedList();
-		Node head = linkedList.insertData(10)
+		Node head = new SinglyLinkedList()
+				.insertData(10)
 				.insertData(20)
 				.insertData(30)
 				.insertData(40)
@@ -22,8 +29,9 @@ public class DeleteAlternateNodes {
 				.insertData(70)
 				.insertData(80)
 				.insertData(90)
+				//.insertData(100)
 				.getHead();
-		System.out.println(linkedList);
+		SinglyLinkedList.traverse(head);
 
 		head = deleteAlternateNodes(head);
 		SinglyLinkedList.traverse(head);
