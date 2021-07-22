b5fabe38d9a020c3128ad318bab3dcda032162ec
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-07-22 16:14:03 +0530

    Make Middle Element Head in Linked List

--- src/com/dataStructure/linkedList/problems/MakeMiddleNodeHeadLinkedList.java
+++ src/com/dataStructure/linkedList/problems/MakeMiddleNodeHeadLinkedList.java
@@ -2,31 +2,31 @@ package com.dataStructure.linkedList.problems;
 
 public class MakeMiddleNodeHeadLinkedList {
 	private static Node makeMiddleNodeHead(Node head) {
+		Node fast = head;
 		Node prev = null;
-		Node node = head;
-		Node secondNode = head;
-		while(secondNode != null && secondNode.next != null) {
-			secondNode = secondNode.next.next;
-			prev = node;
-			node = node.next;
+		Node slow = head;
+		while(fast != null && fast.next != null) {
+			fast = fast.next.next;
+			prev = slow;
+			slow = slow.next;
 		}
 
-		prev.next = node.next;
-		node.next = head;
-		head = node;
+		prev.next = prev.next.next;
+		slow.next = head;
+		head = slow;
 
 		return head;
 	}
 
 	public static void main(String[] args) {
-		SinglyLinkedList linkedList = new SinglyLinkedList();
-		Node head = linkedList.insertData(10)
+		Node head = new SinglyLinkedList()
+				.insertData(10)
 				.insertData(20)
 				.insertData(30)
 				.insertData(40)
 				.insertData(50)
 				.getHead();
-		System.out.println(linkedList);
+		SinglyLinkedList.traverse(head);
 
 		head = makeMiddleNodeHead(head);
 		SinglyLinkedList.traverse(head);
