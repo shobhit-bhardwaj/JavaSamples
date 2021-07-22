d2687af1f916e23fbc1cfce858fafcfa3191c97c
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-07-22 16:33:02 +0530

    Merge at Alternate Position

--- src/com/dataStructure/linkedList/problems/MergeAtAlternatePosition.java
+++ src/com/dataStructure/linkedList/problems/MergeAtAlternatePosition.java
@@ -2,39 +2,40 @@ package com.dataStructure.linkedList.problems;
 
 public class MergeAtAlternatePosition {
 	private static Node mergeAtAlternate(Node head1, Node head2) {
-		Node head = head1;
+		Node node1 = head1;
+		Node node2 = head2;
+		Node temp1, temp2;
+		while(node1 != null && node1.next != null) {
+			temp1 = node1.next;
+			temp2 = node2.next;
 
-		Node pCurr = head1;
-		Node qCurr = head2;
-		while(pCurr != null && qCurr != null) {
-			Node pNext = pCurr.next;
-			Node qNext = qCurr.next;
-			qCurr.next = pNext;
-			pCurr.next = qCurr;
-			pCurr = pNext;
-			qCurr = qNext;
+			node2.next = node1.next;
+			node1.next = node2;
+
+			node1 = temp1;
+			node2 = temp2;
 		}
 
-		return head;
+		return head1;
 	}
 
 	public static void main(String[] args) {
-		SinglyLinkedList linkedList = new SinglyLinkedList();
-		Node head1 = linkedList.insertData(10)
-				.insertData(20)
+		Node head1 = new SinglyLinkedList()
+				.insertData(10)
 				.insertData(30)
-				.insertData(40)
 				.insertData(50)
-				.getHead();
-		System.out.println(linkedList);
-
-		linkedList = new SinglyLinkedList();
-		Node head2 = linkedList.insertData(60)
 				.insertData(70)
-				.insertData(80)
 				.insertData(90)
 				.getHead();
-		System.out.println(linkedList);
+		SinglyLinkedList.traverse(head1);
+
+		Node head2 = new SinglyLinkedList()
+				.insertData(20)
+				.insertData(40)
+				.insertData(60)
+				.insertData(80)
+				.getHead();
+		SinglyLinkedList.traverse(head2);
 
 		Node head = mergeAtAlternate(head1, head2);
 		SinglyLinkedList.traverse(head);
