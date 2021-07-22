9a5346525e977656e613d935db1bdae98f7b60ce
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-07-22 15:56:55 +0530

    Intersection Point of Two Linked List

--- src/com/dataStructure/linkedList/problems/IntersectionPointOfTwoLinkedList.java
+++ src/com/dataStructure/linkedList/problems/IntersectionPointOfTwoLinkedList.java
@@ -2,20 +2,20 @@ package com.dataStructure.linkedList.problems;
 
 public class IntersectionPointOfTwoLinkedList {
 	private static Node intersectionPoint(Node head1, Node head2) {
-		int length1 = 0;
-		for(Node node=head1; node!=null; node=node.next)
-			length1++;
+		int len1 = 0;
+		for(Node temp=head1; temp!=null; temp=temp.next)
+			len1++;
 
-		int length2 = 0;
-		for(Node node=head2; node!=null; node=node.next)
-			length2++;
+		int len2 = 0;
+		for(Node temp=head2; temp!=null; temp=temp.next)
+			len2++;
 
-		int difference = Math.abs(length1 - length2);
-		if(length1 > length2) {
-			for(int i=0; i<difference; i++)
+		int diff = Math.abs(len1 - len2);
+		if(len1 > len2) {
+			for(int i=0; i<diff; i++)
 				head1 = head1.next;
 		} else {
-			for(int i=0; i<difference; i++)
+			for(int i=0; i<diff; i++)
 				head2 = head2.next;
 		}
 
@@ -31,8 +31,8 @@ public class IntersectionPointOfTwoLinkedList {
 	}
 
 	public static void main(String[] args) {
-		SinglyLinkedList linkedList = new SinglyLinkedList();
-		Node head1 = linkedList.insertData(10)
+		Node head1 = new SinglyLinkedList()
+				.insertData(10)
 				.insertData(20)
 				.insertData(30)
 				.insertData(40)
@@ -42,14 +42,14 @@ public class IntersectionPointOfTwoLinkedList {
 				.insertData(80)
 				.insertData(90)
 				.getHead();
-		System.out.println(linkedList);
+		SinglyLinkedList.traverse(head1);
 
-		linkedList = new SinglyLinkedList();
-		Node head2 = linkedList.insertData(10)
+		Node head2 = new SinglyLinkedList()
+				.insertData(10)
 				.insertData(20)
 				.getHead();
 		head2.next.next = head1.next.next.next.next.next.next;
-		System.out.println(linkedList);
+		SinglyLinkedList.traverse(head2);
 
 		Node node = intersectionPoint(head1, head2);
 		System.out.println("Intersection Node - " + node.data);
