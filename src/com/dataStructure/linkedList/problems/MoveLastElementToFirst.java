aeb598519b54e94de941d96e46d6aba17f8340d3
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-07-22 19:54:59 +0530

    Move Last Element to First in Linked List

--- src/com/dataStructure/linkedList/problems/MoveLastElementToFirst.java
+++ src/com/dataStructure/linkedList/problems/MoveLastElementToFirst.java
@@ -2,29 +2,26 @@ package com.dataStructure.linkedList.problems;
 
 public class MoveLastElementToFirst {
 	private static Node moveLastToFirst(Node head) {
-		Node node = head;
-		Node prev = null;
-		while(node.next != null) {
-			prev = node;
-			node = node.next;
-		}
+		Node node;
+		for(node=head; node.next.next!= null; node=node.next);
 
-		node.next = head;
-		prev.next = null;
-		head = node;
+		Node last = node.next;
+		node.next = null;
+		last.next = head;
+		head = last;
 
 		return head;
 	}
 
 	public static void main(String[] args) {
-		SinglyLinkedList linkedList = new SinglyLinkedList();
-		Node head = linkedList.insertData(10)
+		Node head = new SinglyLinkedList()
+				.insertData(10)
 				.insertData(20)
 				.insertData(30)
 				.insertData(40)
 				.insertData(50)
 				.getHead();
-		System.out.println(linkedList);
+		SinglyLinkedList.traverse(head);
 
 		head = moveLastToFirst(head);
 		SinglyLinkedList.traverse(head);
