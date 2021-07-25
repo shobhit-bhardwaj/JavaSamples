ed3cd9225447056840b77464af7e46a3d434e0bd
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-07-25 21:02:41 +0530

    Rearrange Linked List in Even Odd Position

--- src/com/dataStructure/linkedList/problems/RearrangeLinkedListEvenOddPosition.java
+++ src/com/dataStructure/linkedList/problems/RearrangeLinkedListEvenOddPosition.java
@@ -3,29 +3,33 @@ package com.dataStructure.linkedList.problems;
 public class RearrangeLinkedListEvenOddPosition {
 	private static Node rearrange(Node head) {
 		Node odd = head;
+		Node evenHead = head.next;
 		Node even = head.next;
-		Node evenFirst = even;
+
+		//while(odd != null && odd.next != null && even.next != null) {
 		while(odd.next != null && odd.next.next != null) {
 			odd.next = odd.next.next;
-			even.next = even.next.next;
 			odd = odd.next;
+
+			even.next = even.next.next;
 			even = even.next;
 		}
-		odd.next = evenFirst;
+
+		odd.next = evenHead;
 
 		return head;
 	}
 
 	public static void main(String[] args) {
-		SinglyLinkedList linkedList = new SinglyLinkedList();
-		Node head = linkedList.insertData(10)
+		Node head = new SinglyLinkedList()
+				.insertData(10)
 				.insertData(20)
 				.insertData(30)
 				.insertData(40)
 				.insertData(50)
-				.insertData(60)
+				//.insertData(60)
 				.getHead();
-		System.out.println(linkedList);
+		SinglyLinkedList.traverse(head);
 
 		head = rearrange(head);
 		SinglyLinkedList.traverse(head);
