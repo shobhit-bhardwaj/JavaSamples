dd5579e77050636f4ae6e4ee6473e04f424afbfa
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-07-26 13:10:56 +0530

    Remove Duplicate in UnSorted Linked List

--- src/com/dataStructure/linkedList/problems/RemoveDuplicateInUnsortedLinkedList.java
+++ src/com/dataStructure/linkedList/problems/RemoveDuplicateInUnsortedLinkedList.java
@@ -2,28 +2,21 @@ package com.dataStructure.linkedList.problems;
 
 public class RemoveDuplicateInUnsortedLinkedList {
 	private static Node removeDuplicateUsingTwoLoops(Node head) {
-		Node node1 = head;
-		Node node2;
-
-		while(node1.next != null) {
-			node2 = node1;
-
-			while(node2.next != null) {
+		for(Node node1 = head; node1.next!=null; node1=node1.next) {
+			for(Node node2 = node1; node2.next!=null;) {
 				if(node1.data == node2.next.data) {
 					node2.next = node2.next.next;
 				} else
 					node2 = node2.next;
 			}
-
-			node1 = node1.next;
 		}
 
 		return head;
 	}
 
 	public static void main(String[] args) {
-		SinglyLinkedList linkedList = new SinglyLinkedList();
-		Node head = linkedList.insertData(40)
+		Node head = new SinglyLinkedList()
+				.insertData(40)
 				.insertData(20)
 				.insertData(40)
 				.insertData(10)
@@ -38,7 +31,7 @@ public class RemoveDuplicateInUnsortedLinkedList {
 				.insertData(60)
 				.insertData(60)
 				.getHead();
-		System.out.println(linkedList);
+		SinglyLinkedList.traverse(head);
 
 		head = removeDuplicateUsingTwoLoops(head);
 		SinglyLinkedList.traverse(head);
