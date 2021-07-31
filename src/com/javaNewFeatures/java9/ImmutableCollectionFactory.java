9a1309b789c0a85f5071b6a620f1fa3c9ddd0142
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-07-31 22:50:20 +0530

    Immutable Collection Factory Method

--- src/com/javaNewFeatures/java9/ImmutableCollectionFactory.java
+++ src/com/javaNewFeatures/java9/ImmutableCollectionFactory.java
@@ -12,10 +12,10 @@ public class ImmutableCollectionFactory {
 		System.out.println("asList After set at position 2 - " + asList);
 
 		List<Integer> ofList = List.of(1, 2, 3, 4, 5);
-		System.out.println("ofList - "+ofList);
+		System.out.println("ofList - " + ofList);
 		//	ofList.set(2, 7);		//	Gives java.lang.UnsupportedOperationException (Immutable Collection)
 
 		Map<Integer, String> ofMap = Map.of(1, "AAA", 2, "BBB", 3, "CCC");
-		System.out.println("ofMap - "+ofMap);
+		System.out.println("ofMap - " + ofMap);
 	}
 }
