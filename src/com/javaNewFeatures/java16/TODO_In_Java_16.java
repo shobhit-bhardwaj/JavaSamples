2f58184fa81cc655f001fe6ff8afbe96a44d477d
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-08-01 21:37:55 +0530

    TODO In Java 16

---
+++ src/com/javaNewFeatures/java16/TODO_In_Java_16.java
@@ -0,0 +1,17 @@
+package com.javaNewFeatures.java16;
+
+//	TO-DO
+
+/*
+Records
+Switch Expressions
+Text Blocks
+Helpful NullPointerExceptions 
+Sealed Classes
+Hidden Classes
+Pattern Matching Type Checks
+*/
+
+public class TODO_In_Java_16 {
+	
+}
