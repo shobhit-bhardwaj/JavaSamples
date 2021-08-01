779e7fa9323e9e79574d4068c42a0b025b78a788
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-08-01 19:15:08 +0530

    Calculate Elapsed Time in Instant

---
+++ src/com/javaNewFeatures/java8/examples/CalculateElapsedTime.java
@@ -0,0 +1,17 @@
+package com.javaNewFeatures.java8.examples;
+
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+
+public class CalculateElapsedTime {
+	public static void main(String[] args) throws Exception {
+		long time1 = Instant.now().toEpochMilli();
+
+		TimeUnit.SECONDS.sleep(3);
+
+		long time2 = Instant.now().toEpochMilli();
+
+		long difference = time2 - time1;
+		System.out.println("Elapsed Time In Millis - " + difference);
+	}
+}
