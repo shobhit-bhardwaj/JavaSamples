7b23ee3035491ea346b99fdb1ffdb5dbb40469c0
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-08-01 19:22:29 +0530

    Convert Date to LocalDateTime

---
+++ src/com/javaNewFeatures/java8/examples/ConvertDateToLocalDateTime.java
@@ -0,0 +1,22 @@
+package com.javaNewFeatures.java8.examples;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class ConvertDateToLocalDateTime {
+	public static void main(String[] args) {
+		Date date = new Date();
+		LocalDateTime dateTime = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+		System.out.println("Date - " + date);
+		System.out.println("Date to LocalDateTime - " + dateTime);
+		System.out.println();
+
+		dateTime = LocalDateTime.of(2018, 02, 20, 12, 00, 00);
+		date = Date.from(dateTime.atZone(ZoneId.systemDefault()).toInstant());
+
+		System.out.println("LocalDateTime - " + dateTime);
+		System.out.println("LocalDateTime to Date - " + date);
+	}
+}
