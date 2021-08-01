90ac09f95ad68f2d0aebd83bf8b2d280943953d7
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-08-01 21:31:51 +0530

    Temporal Adjusters Example

---
+++ src/com/javaNewFeatures/java8/examples/TemporalAdjustersExample.java
@@ -0,0 +1,25 @@
+package com.javaNewFeatures.java8.examples;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+
+public class TemporalAdjustersExample {
+	public static void main(String[] args) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd");
+		LocalDate date = LocalDate.of(2018, 02, 15);
+		System.out.println("LocalDateTime - " + formatter.format(date));
+
+		System.out.println();
+
+		LocalDate temporalDate = date.with(TemporalAdjusters.firstDayOfMonth());
+		System.out.println("First Day of Month - " + formatter.format(temporalDate));
+
+		temporalDate = date.with(TemporalAdjusters.lastDayOfMonth());
+		System.out.println("Last Day of Month - " + formatter.format(temporalDate));
+
+		temporalDate = date.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+		System.out.println("Next Monday - " + formatter.format(temporalDate));
+	}
+}
