8ff132d7411ba0796486a29a382b42fe131e2096
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-08-03 12:01:09 +0530

    Convert Instant to LocalDateTime

---
+++ src/com/javaBasics/ConvertInstantToLocalDateTime.java
@@ -0,0 +1,15 @@
+package com.javaNewFeatures.java8.examples;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class ConvertInstantToLocalDateTime {
+	public static void main(String[] args) {
+		Instant instant = Instant.now();
+		LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneOffset.UTC);
+
+		System.out.println("Instant - " + instant);
+		System.out.println("LocalDateTime - " + dateTime);
+	}
+}
