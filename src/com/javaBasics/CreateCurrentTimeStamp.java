123b6bac7d8ae7557db4d4e5c5ace91db7a6a2ac
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-08-03 12:15:46 +0530

    Create Current TimeStamp

---
+++ src/com/javaBasics/CreateCurrentTimeStamp.java
@@ -0,0 +1,17 @@
+package com.javaNewFeatures.java8.examples;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+public class CreateCurrentTimeStamp {
+	public static void main(String[] args) {
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+		Instant instant = timestamp.toInstant();
+
+		System.out.println("Instant - " + instant);
+		System.out.println("To Epoch - " + instant.toEpochMilli());
+
+		timestamp = Timestamp.from(instant);
+		System.out.println("Get Time from Timestamp - " + timestamp.getTime());
+	}
+}
