43b10384e7938c84306098cacff80f5b7c929754
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-08-03 12:25:21 +0530

    GroupBy in Stream Examples

---
+++ src/com/javaBasics/StreamGroupByExample.java
@@ -0,0 +1,76 @@
+package com.javaBasics;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class StreamGroupByExample {
+
+	private static class Employ {
+		private int id;
+		private String name;
+		private int salary;
+
+		public Employ(int id, String name, int salary) {
+			this.id = id;
+			this.name = name;
+			this.salary = salary;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public int getSalary() {
+			return salary;
+		}
+
+		@Override
+		public String toString() {
+			return "Employ [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+		}
+	}
+
+	public static void main(String[] args) {
+		//	Group By String Values
+		List<String> names = List.of("Shobhit", "Ravi", "Shobhit", "Rajesh", "Shobhit", "Ravi", "Rajesh", "Shobhit", "Ravi");
+		Map<String, Long> nameResults = names.stream()
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		System.out.println("Group By List of Strings - " + nameResults);
+
+		System.out.println();
+
+		//	Group By List of Objects
+		List<Employ> employs = List.of(
+				new Employ(101, "Shobhit", 17000),
+				new Employ(102, "Ravi", 12000),
+				new Employ(103, "Rajesh", 15000),
+				new Employ(101, "Shobhit", 17000),
+				new Employ(102, "Ravi", 12000),
+				new Employ(101, "Shobhit", 17000),
+				new Employ(103, "Rajesh", 15000));
+
+		nameResults = employs.stream().
+				collect(Collectors.groupingBy(Employ :: getName, Collectors.counting()));
+		System.out.println("Group By List of Objects - " + nameResults);
+
+		System.out.println();
+
+		//	Group By List of Objects (on Basis of Id)
+		Map<Integer, List<Employ>> employMap = employs.stream()
+				.collect(Collectors.groupingBy(Employ :: getId));
+		System.out.println("Group By List of Objects (on Basis of Id) - " + employMap);
+
+		System.out.println();
+
+		//	Group By List of Objects, Sum Salary
+		Map<String, Integer> sumSalaryResult = employs.stream()
+				.collect(Collectors.groupingBy(Employ :: getName, Collectors.summingInt(Employ :: getSalary)));
+		System.out.println("Group By List of Objects (Sum Salary) - " + sumSalaryResult);
+	}
+}
