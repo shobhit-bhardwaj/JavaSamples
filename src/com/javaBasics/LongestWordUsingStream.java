6ab7f36fbf6a98040ac97cff5f199bb175180d53
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-08-03 12:24:25 +0530

    Find Longest Word in Sentence Using Stream

---
+++ src/com/javaBasics/LongestWordUsingStream.java
@@ -0,0 +1,17 @@
+package com.javaBasics;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class LongestWordUsingStream {
+	public static void main(String[] args) {
+		String line = "Hello my name is Shobhit and I am an Engineer";
+
+		String longestWord = Arrays
+				.stream(line.split(" "))
+				.max(Comparator.comparingInt(String :: length))
+				.orElse(null);
+
+		System.out.println("Longest Word - " + longestWord);
+	}
+}
