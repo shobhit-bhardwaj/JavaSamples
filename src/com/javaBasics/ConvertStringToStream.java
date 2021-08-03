d432c76dc3e02cff3e5ee5dd04e345710c644725
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-08-03 12:01:55 +0530

    Convert String to Stream

---
+++ src/com/javaBasics/ConvertStringToStream.java
@@ -0,0 +1,11 @@
+package com.javaNewFeatures.java8.examples;
+
+public class ConvertStringToStream {
+	public static void main(String[] args) {
+		String name = "Shobhit Bhardwaj";
+
+		name.chars()
+				.mapToObj(x -> (char)x)
+				.forEach(System.out :: println);
+	}
+}
