9ff3b37c40891dfa0bd9d84d2e269b691d8b9b33
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-10-03 14:53:53 +0530

    Java Clone Basic

--- src/com/javaBasics/CloneTest.java
+++ src/com/javaBasics/CloneTest.java
@@ -1,96 +1,77 @@
 package com.javaBasics;
 
-class Department implements Cloneable {
-	private String departmentName;
-
-	public Department(String departmentName) {
-		this.departmentName = departmentName;
-	}
-
-	public String getDepartmentName() {
-		return departmentName;
-	}
-
-	public void setDepartmentName(String departmentName) {
-		this.departmentName = departmentName;
-	}
-
-	@Override
-	protected Object clone() throws CloneNotSupportedException {
-		return super.clone();
-	}
-
-	@Override
-	public String toString() {
-		return "Department [departmentName=" + departmentName + "]";
-	}
-}
-
-class Employ implements Cloneable {
-	private int id;
-	private String name;
-	private Department department;
-
-	public Employ(int id, String name, Department department) {
-		this.id = id;
-		this.name = name;
-		this.department = department;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Department getDepartment() {
-		return department;
-	}
-
-	public void setDepartment(Department department) {
-		this.department = department;
-	}
-
-	@Override
-	protected Object clone() throws CloneNotSupportedException {
-		//	Shallow Cloning
-		//return super.clone();
-
-		//	Deep Cloning
-		Employ clone = (Employ) super.clone();
-		clone.setDepartment((Department) clone.getDepartment().clone());
-		return clone;
-	}
-
-	@Override
-	public String toString() {
-		return "Employ [id=" + id + ", name=" + name + ", department=" + department + "]";
-	}
-}
-
 public class CloneTest {
+
+	private static class Department implements Cloneable {
+		private String departmentName;
+
+		public Department(String departmentName) {
+			this.departmentName = departmentName;
+		}
+
+		public void setDepartmentName(String departmentName) {
+			this.departmentName = departmentName;
+		}
+
+		@Override
+		protected Object clone() throws CloneNotSupportedException {
+			return super.clone();
+		}
+
+		@Override
+		public String toString() {
+			return "Department [departmentName=" + departmentName + "]";
+		}
+	}
+
+	private static class Employ implements Cloneable {
+		private int id;
+		private String name;
+		private Department department;
+
+		public Employ(int id, String name, Department department) {
+			this.id = id;
+			this.name = name;
+			this.department = department;
+		}
+
+		public Department getDepartment() {
+			return department;
+		}
+
+		public void setDepartment(Department department) {
+			this.department = department;
+		}
+
+		@Override
+		protected Object clone() throws CloneNotSupportedException {
+			//	Shallow Cloning
+			//return super.clone();
+
+			//	Deep Cloning
+			Employ clone = (Employ) super.clone();
+			clone.setDepartment((Department) clone.getDepartment().clone());
+			return clone;
+		}
+
+		@Override
+		public String toString() {
+			return "Employ [id=" + id + ", name=" + name + ", department=" + department + "]";
+		}
+	}
+
 	public static void main(String[] args) throws CloneNotSupportedException {
 		Department department = new Department("Java");
 
 		Employ employ = new Employ(101, "Shobhit Bhardwaj", department);
-		System.out.println("Employ - "+employ);
+		System.out.println("Employ - " + employ);
 		Employ employClone = (Employ) employ.clone();
-		System.out.println("Employ Clone - "+employClone);
+		System.out.println("Employ Clone - " + employClone);
 
 		employ.getDepartment().setDepartmentName("J2EE");
 
-		System.out.println("Employ - "+employ);
-		System.out.println("Employ Clone - "+employClone);
+		System.out.println("Employ - " + employ);
+		System.out.println("Employ Clone - " + employClone);
 
 		/*
 		 * Deep Cloning can be Achieved By Following Techniques -
