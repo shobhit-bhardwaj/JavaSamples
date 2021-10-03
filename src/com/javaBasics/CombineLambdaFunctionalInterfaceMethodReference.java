f254db4644a73b40d5e150121a0bf1e0be9e03a4
Shobhit Bhardwaj <successshobhit@gmail.com>
2021-10-03 14:56:22 +0530

    Combine Lambda Functional Interface and Method Reference

--- src/com/javaBasics/CombineLambdaFunctionalInterfaceMethodReference.java
+++ src/com/javaBasics/CombineLambdaFunctionalInterfaceMethodReference.java
@@ -21,13 +21,30 @@ public class CombineLambdaFunctionalInterfaceMethodReference {
 		}
 	}
 
+	private static boolean filter(Employ employ) {
+		return employ.name.startsWith("S");
+	}
+
+	private static void print(Employ employ) {
+		System.out.println(employ);
+	}
+
 	public static void main(String[] args) {
 		List<Employ> employs = List.of(
 				new Employ(101, "Shobhit"),
 				new Employ(102, "Ravi"),
 				new Employ(103, "Rajesh"));
 
+		//	Approach 1
 		performAction(employs, employ -> employ.name.startsWith("R"), System.out :: println);
+
+		//	Approach 2
+		performAction(employs, CombineLambdaFunctionalInterfaceMethodReference :: filter, CombineLambdaFunctionalInterfaceMethodReference :: print);
+
+		//	Approach 3
+		Predicate<Employ> predicate = employ -> employ.name.startsWith("S");
+		Consumer<Employ> consumer = employ -> System.out.println(employ);
+		performAction(employs, predicate, consumer);
 	}
 
 	private static void performAction(List<Employ> employs, Predicate<Employ> predicate, Consumer<Employ> consumer) {
