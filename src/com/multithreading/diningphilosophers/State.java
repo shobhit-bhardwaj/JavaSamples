d928c92a7d35eb6e75d625e074bd2951143f8d45
Shobhit Bhardwaj <successshobhit@gmail.com>
2022-01-05 13:47:34 +0530

    Moving Resources in Other Repository

--- src/com/multithreading/diningphilosophers/State.java
+++
@@ -1,5 +0,0 @@
-package com.multithreading.diningphilosophers;
-
-public enum State {
-	LEFT, RIGHT
-}
--- src/com/multithreading/studentlibrary/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.multithreading.studentlibrary;
-
-public class Constant {
-	public static final int NO_OF_BOOKS = 7;
-	public static final int NO_OF_STUDENTS = 5;
-
-	public static final int READING_TIME = 2;	//	in seconds
-	public static final int TOTAL_SIMULATION_TIME = 10;	//	in seconds
-}
